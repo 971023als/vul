2d7db4bc46393dd8b2f905a0f7c8d2c761199aac
971023als <971023als@naver.com>
2023-01-09 13:58:43 +0900

    Update check_perm.sh

--- check_perm.sh
+++ check_perm.sh
@@ -1,30 +1,45 @@
 #!/bin/bash
 
-PermList=perm.list
-ResultFile=report.txt
+. functions.sh
+
+
+# 1) 변수 설정 및 초기화
+PermList=perm.list # 점검 대상 파일 목록
+Result=report2.txt # 점검 결과
 TMP1=/tmp/tmp1
 
-> $ResultFile
-> $TMP1
+> $Result # 파일 실행 시 점검 결과 초기화
+> $TMP1 # 파일 실행 시 임시 파일 초기화
 
-
-cat $PermList | while read FileName FileAuth FilePermNum FilePermChar
+# 2) 파일 유무 점검
+for ChkFile in `cat $PermList | awk '{print $1}'`
 do
-		#echo "$FileName : $FilePermNum : $FilePermChar"
-		if	[ ! -f $FileName ] ; then 
-				echo "[ ERROR ] $FileName not found" >> $ResultFile
-				continue
-		else
-				find $FileName -type f -perm -$FilePermNum -ls \
-				| fgrep -v $FilePermChar > $TMP1 
-				if [ -s $TMP1 ] ; then
-						BadPerm=$(cat $TMP1 | awk '{print $3}')
-						echo "[ WARN ] $FilePermNum 이하 : $BadPerm : $FileName" >> $ResultFile
-				else
-						GoodPerm=$(find $FileName -type f -ls | awk '{print $3}')
-						echo "[  OK  ] $FilePermNum 이하 : $GoodPerm : $FileName" >> $ResultFile
-				fi
-		fi
-done	
+if [ ! -f $ChkFile ] ; then
+echo "[ WARN ] '$ChkFile' not found."
+exit 1
+break
+fi
+done
+
+
+# 3) 점검 스크립트
+cat $PermList | while read FName FAuth FPermNum FPermChar
+do
+if [ ! -f $FName ] ; then
+echo "[ ERROR ] $FName not found" >> $Result
+echo >> $Result
+continue
+else
+find $FName -type f -perm -$FPermNum -ls | fgrep -v $FPermChar > $TMP1
+if [ -s $TMP1 ] ; then
+BadPerm=$(cat $TMP1 | awk '{print $3}')
+WARN "$FPermNum : $FName ($BadPerm)" >> $Result
+else
+GoodPerm=$(find $FName -type f -ls | awk '{print $3}')
+OK "$FPermNum : $FName ($GoodPerm)" >> $Result
+fi
+fi
+done
+[출처] 프로젝트) Linux 서버 취약점 점검 스크립트 제작|작성자 takudaddy
 
  
