954f37ef65e923173d9a03fdf86eb39e2e5e5a0e
971023als <971023als@naver.com>
2023-01-10 12:43:41 +0900

    r

--- U-07.sh
+++ U-07.sh
@@ -30,37 +30,36 @@ CHECK_PERM=$(find /etc/passwd -type f -perm -644 -ls | grep -v rw-r--r-- | awk '
 
 CHECK_PERM2=$(ls -l /etc/passwd | awk '{print $1}')
 
- 
 
+ 
 if [ -f $CHECK_FILE ] ; then
 
 INFO "$CHECK_FILE 파일이 존재하며 소유자와 권한을 체크합니다."
 
-if [ $CHOWN = 'root' ] ; then
+# Check permissions and ownership of /etc/passwd
+permissions=$(stat -c "%a" /etc/passwd)
+if [ $permissions -le 644 ]
+then
+  echo "/etc/passwd에 적절한 권한이 있음"
+else
+  echo "/etc/passwd에 잘못된 사용 권한이 있습니다"
+fi
 
-OK "파일의 소유자가 root 입니다."
+owner=$(ls -l /etc/passwd | awk '{print $3}')
+if [ $owner == "root" ]
+then
+  echo "/etc/passwd는 root에 의해 소유됩니다"
+else
+  echo "/etc/passwd가 루트에 의해 소유되지 않음"
+fi
 
-if [ $CHECK_PERM2 > 644 ] ; then
 
-WARN "파일의 권한이 644 이상으로 되어 있습니다."
 
 echo
 
 echo "$CHECK_FILE 의 권한이 $CHECK_PERM2 으로 되어 있습니다." > $TMP1
 
 INFO "권한 설정 상태는 $TMP1 파일에서 확인하세요."
-
-else
-
-OK "파일의 권한이 644 이하로 되어 있습니다."
-
-fi
-
-else
-
-WARN "파일의 소유자가 root가 아닙니다."
-
-fi
 
 else
 
