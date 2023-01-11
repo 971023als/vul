6b3e050a905b182d56b3b2f0cdaf9197e2148a8d
971023als <971023als@naver.com>
2023-01-11 15:32:25 +0900

    Update U-72.sh

--- U-72.sh
+++ U-72.sh
@@ -22,21 +22,31 @@ EOF
 
 BAR
 
- 
 
-# Check Server Tokens setting
-if grep -q "ServerTokens Prod" /etc/apache2/conf-enabled/security.conf; then
-    OK "서버 토큰 설정이 Prod로 설정되어 있습니다."
-else
-    WARN "서버 토큰 설정이 Prod로 설정이 안 되어 있습니다"
-fi
+# Define the log files to check
+log_files=(/var/log/auth.log /var/log/secure /var/log/syslog)
 
-# Check Server Signature setting
-if grep -q "ServerSignature Off" /etc/apache2/conf-enabled/security.conf; then
-    OK "Server Signature 설정이 Off로 설정되어 있습니다"
-else
-    WARN "Server Signature 설정이 Off로 설정이 안 되어 있습니다"
-fi
+# Define the security policies to check
+policies=("log all authentication attempts" "log all system-level access" "log all user-level access")
+
+# Check if log recording policies have been established
+for file in "${log_files[@]}"; do
+    if [ ! -f $file ]; then
+        echo "$file에 대해 설정된 로그 기록 정책이 없습니다"
+    else
+        echo "$file에 대해 설정된 로그 기록 정책이 있습니다"
+    fi
+done
+
+# Check if logs are being left in compliance with established security policies
+for policy in "${policies[@]}"; do
+    if grep -q "$policy" /etc/rsyslog.conf; then
+        echo "로그가 $policy를 준수하도록 남겨지고 있습니다."
+    else
+        echo "로그가 $policy를 준수하여 남아 있지 않습니다."
+    fi
+done
+
 
 cat $RESULT
 
