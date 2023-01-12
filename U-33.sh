9c94f3ee85f68cda2355b887257d0f59d8f44e29
971023als <971023als@naver.com>
2023-01-12 14:49:18 +0900

    Update U-33.sh

--- U-33.sh
+++ U-33.sh
@@ -23,23 +23,22 @@ EOF
 
 BAR
 
-ps='ps -ef | grep named | grep -v grep | wc-l'
-ver='named -v 2>dev/null | awk -F " " '{print $2}' | awk -F "ubuntu" '{print $1}' | awk -F "-" '{print $1}' | tr -d "."'
-inst='named -v 2>/dev/null | wc-l'
+ps_chk=`ps -ef | grep named | grep -v grep | wc -l`
+ver_chk=`named -v 2>/dev/nill | awk -F " " '{print $2}' | awk -F "ubuntu" '{print $1}' | awk -F "-" '{print $1}' | tr -d "."`
+inst_chk=`named -v 2>/dev/null | wc -l`
 version=9113
 
-# bind 9 서비스가 설치되어 있고 작동하는지 확인
+#bind9 서비스가 설치되어있고 작동하는지 확인.
+if [ $ps_chk != 0 ] && [ $inst_chk != 0 ];then
 
-if [ $ps != 0 ] && [ $inst_chk !=0 ];then
-
-      #설정한 버전과 bind9 버전 비교
-      if [ $ver -lt $version ]; then
-            WARN " 바인드 버전이 취약한 버전입니다."
-      else
-            OK " 바인드 버전이 취약한 버전이 아닙니다."
-      fi
+	#설정한 버전과 bind9 버전 비교
+	if [ $ver_chk -lt $version ];then
+		echo "[-] U33. Bind version is vulnerable, Result : Vul" 
+	else
+		echo "[+] U33. Bind version is very well, Result : Good"
+	fi
 else
-      INFO " 바인드 버전이 없습니다."
+	echo "[+] U33. Bind service disable, Result : Good"
 fi
 
 
