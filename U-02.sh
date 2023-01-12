a036d8e577b57824aad276e9050bdfe2f144bd1e
971023als <971023als@naver.com>
2023-01-12 15:27:28 +0900

    Update U-02.sh

--- U-02.sh
+++ U-02.sh
@@ -26,18 +26,117 @@ EOF
 BAR
 
 
-# Check for minimum password length
-min_length=$(grep "^password.*sufficient.*pam_unix.so.*minlen" /etc/login.defs | awk '{print $9}')
-if [ $min_length -lt 8 ]; then
-    WARN "최소 암호 길이가 8자 미만입니다."
+NFO $TMP1 파일을 점검한다
+
+echo >> $TMP1
+
+echo
+
+echo "다음은 /etc/security/pwquality.conf 파일을 해석할 때 사용하는 내용입니다"
+
+echo "=============================================================">> $TMP1
+
+cat /etc/security/pwquality.conf | egrep -v '(^$|^#)' >> $TMP1
+
+ 
+
+ 
+
+cat << EOF >> $TMP1
+
+=============================================================
+
+다음은 /etc/security/pwquality.conf 파일을 해석할 때 사용하는 내용입니다.
+
+ 
+
+minlen : 패스워드 최소 길이입니다.
+
+minclass : 패스워드 class 지정입니다.
+
+lcredit : 패스워드 소문자 포함 지정입니다.
+
+ucredit : 패스워드 대문자 포함 지정입니다.
+
+dcredit : 패스워드 숫자 포함 지정입니다.
+
+ocredit : 패스워드 특수문자 포함 지정입니다.
+
+=============================================================
+
+다음은 /etc/security/pwquality.conf 파일의 내용이 없으면,
+
+ 
+
+1) 기본값을 사용하는 경우입니다. 이 경우, 패스워드 정책에 맞지 않습니다. 
+
+따라서, 반드시 정책을 변경할 것을 권장합니다.
+
+2) 정책을 변경하는 경우에는 다음과 같은 명령어를 통해 설정할 수 있습니다.
+
+# authconfig --passminlen=8 --passminclass=3 
+
+--enablereqlower --disablerequpper --enablereqdigit 
+
+--enablereqother --update
+
+=============================================================
+
+EOF
+
+ 
+indPatternReturnValue() {
+# $1 : File name
+# $2 : Find Pattern
+if egrep -v '^#|^$' $1 | grep -q $2 ; then # -q = 출력 내용 없도록
+	ReturnValue=$(egrep -v '^#|^$' $1 | grep $2 | awk -F= '{print $2}')
+else
+	ReturnValue=None
+fi
+echo $ReturnValue
+}
+ 
+ 
+
+PWQUALITY_CONF=/etc/security/pwquality.conf.copy
+
+VALUE1=$(FindPatternReturnValue $PWQUALITY_CONF minlen)
+
+if [ $VALUE1 = None ] ; then
+
+WARN '패스워드의 최소 길이 설정이 8 글자 미만으로 되어 있음!'
+
+else
+
+if [ $VALUE1 -ge 12 ] ; then
+
+Ret1=$(IsFindPattern $PWQUALITY_CONF dcredit)
+
+Ret2=$(IsFindPattern $PWQUALITY_CONF ucredit)
+
+Ret3=$(IsFindPattern $PWQUALITY_CONF lcredit)
+
+Ret4=$(IsFindPattern $PWQUALITY_CONF ocredit)
+
+if [ $Ret1 -eq 0 -a $Ret2 -eq 0 -a $Ret3 -eq 0 -a $Ret4 -eq 0 ] ; then
+
+OK "영문 숫자 특수문가자 혼합된 8글자 이상의 패스워드를 사용하고 있습니다."
+
+else
+
+WARN "패스워드가 8글자 이상이지만 dcredit|ucredit|lcredit|ocredit 중 
+
+설정이 없는 것이 존재합니다." 
+
 fi
 
-# Check for minimum input of special characters, digits, and letters
-cracklib=$(grep "^password.*sufficient.*pam_cracklib.so.*difok" /etc/login.defs | awk '{print $9}')
-if [ $cracklib -lt 3 ]; then
-    WARN "특수 문자, 숫자 및 문자의 최소 입력이 3 미만입니다."
+else
+
+WARN '패스워드의 최소 길이 설정이 8글자 미만으로 되어 있음'
+
 fi
-OK "비밀번호 정책은 최소 길이 8자, 영문, 숫자, 특수 문자 입력으로 설정됩니다."
+
+fi
 
 
  
