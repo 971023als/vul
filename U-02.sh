b75e8b7b9839b42adf51f3f9e9a191f4390cc76c
971023als <971023als@naver.com>
2023-01-12 10:28:16 +0900

    j

--- U-02.sh
+++ U-02.sh
@@ -25,82 +25,19 @@ EOF
 
 BAR
 
- 
 
- 
-
-INFO $TMP1 파일을 점검한다
-
-echo >> $TMP1
-
-echo
-
-echo "다음은 /etc/login.defs 파일을 해석할 때 사용하는 내용입니다"
-
-echo "=============================================================">> $TMP1
-
-cat /etc/login.defs | egrep -v '(^$|^#)' >> $TMP1
-
- 
-
- 
-
-cat << EOF >> $TMP1
-
-=============================================================
-
-다음은 /etc/login.defs 파일을 해석할 때 사용하는 내용입니다.
-
- 
-
-minlen : 패스워드 최소 길이입니다.
-
-minclass : 패스워드 class 지정입니다.
-
-lcredit : 패스워드 소문자 포함 지정입니다.
-
-ucredit : 패스워드 대문자 포함 지정입니다.
-
-dcredit : 패스워드 숫자 포함 지정입니다.
-
-ocredit : 패스워드 특수문자 포함 지정입니다.
-
-=============================================================
-
-다음은 /etc/login.defs 파일의 내용이 없으면,
-
- 
-
-1) 기본값을 사용하는 경우입니다. 이 경우, 패스워드 정책에 맞지 않습니다. 
-
-따라서, 반드시 정책을 변경할 것을 권장합니다.
-
-2) 정책을 변경하는 경우에는 다음과 같은 명령어를 통해 설정할 수 있습니다.
-
-# authconfig --passminlen=8 --passminclass=3 
-
---enablereqlower --disablerequpper --enablereqdigit 
-
---enablereqother --update
-
-=============================================================
-
-EOF
-
-# Check the minimum password length
-min_length=$(grep "^password.*required.*pam_cracklib.so.*minlen" /etc/pam.d/common-password | awk '{print $4}')
-if [ $min_length -ge 8 ]; then
-    OK "최소 암호 길이가 8 이상으로 설정됨"
-else
-    WARN "최소 암호 길이가 8 이상으로 설정되지 않음"
+# Check for minimum password length
+min_length=$(grep "^password.*sufficient.*pam_unix.so.*minlen" /etc/pam.d/common-password | awk '{print $9}')
+if [ $min_length -lt 8 ]; then
+    WARN "최소 암호 길이가 8자 미만입니다."
 fi
 
-# Check if the use of uppercase and special characters is required
-if grep -q "^password.*required.*pam_cracklib.so.*ucredit" /etc/pam.d/common-password && grep -q "^password.*required.*pam_cracklib.so.*dcredit" /etc/pam.d/common-password; then
-    ok "영문 문자, 숫자, 특수 문자의 최소 입력 기능이 설정되었습니다."
-else
-    WARN "영문 문자, 숫자, 특수 문자의 최소 입력 기능이 설정되지 않았습니다."
+# Check for minimum input of special characters, digits, and letters
+cracklib=$(grep "^password.*sufficient.*pam_cracklib.so.*difok" /etc/pam.d/common-password | awk '{print $9}')
+if [ $cracklib -lt 3 ]; then
+    WARN "특수 문자, 숫자 및 문자의 최소 입력이 3 미만입니다."
 fi
+OK "비밀번호 정책은 최소 길이 8자, 영문, 숫자, 특수 문자 입력으로 설정됩니다."
 
 
  
--- U-03.sh
+++ U-03.sh
@@ -16,23 +16,31 @@ CODE [U-03] 계정 잠금 임계값 설정
 
 cat << EOF >> $result
 
-[양호]: 계정 잠금 임계값이 5이하의 값으로 설정되어 있는 경우
+[양호]: 계정 잠금 임계값이 10회 이하의 값으로 설정되어 있는 경우
 
-[취약]: 계정 잠금 임계값이 설정되어 있지 않거나, 5이하의 값으로 설정되지 않은 경우
+[취약]: 계정 잠금 임계값이 설정되어 있지 않거나, 10회 이하의 값으로 설정되지 않은 경우
 
 EOF
 
 BAR
 
 
-# Check if the pam_tally2 module is in use
-if ! grep -q "pam_tally2" /etc/pam.d/common-auth; then
-    INFO "https_https2 모듈이 사용 중이 아닙니다."
-elif grep -q "^auth.*required.*pam_tally2.so.*onerr=fail.*deny=5" /etc/pam.d/common-auth; then
-    ok "계정 잠금 임계값이 5회로 설정되었습니다."
-else
-    WARN "계정 잠금 임계값이 설정되지 않았거나 5보다 큰 값으로 설정되지 않았습니다."
+# Get the current account lock threshold
+threshold=$(grep "^auth.*pam_tally2.so.*onerr=fail.*silent.*deny=.*lock_time=" /etc/pam.d/common-auth | awk '{print $9}' | awk -F "=" '{print $2}')
+
+# Check if the threshold is not set
+if [ -z "$threshold" ]; then
+  WARN "Error: 계정 잠금 임계값이 설정되지 않았습니다"
 fi
+
+# Check if the threshold is greater than 10
+if [ "$threshold" -gt 10 ]; then
+  WARN "Error: 계정 잠금 임계값이 $threshold로 설정되었습니다. 10 이하여야 합니다"
+fi
+
+# If the script reaches this point, the threshold is set to a value of 10 or less
+OK "계정 잠금 임계값이 $threshold(필요에 따라 10 이하)로 설정되었습니다."
+
 
 
 
