e84ba46f4964b07418371aff6ed8b952e1be2064
971023als <971023als@naver.com>
2023-02-03 16:29:42 +0900

    Update U-18.sh

--- U-18.sh
+++ U-18.sh
@@ -30,17 +30,19 @@ iptables -S
 INFO "특정 규칙에 대한 입력 체인 확인"
 iptables -C INPUT -p tcp --dport 22 -j ACCEPT
 if [ $? -eq 0 ]; then
-  OK "수신 SSH 트래픽(포트 22)을 허용하는 규칙이 INPUT 체인에 있습니다"
-else
-  WARN "수신 SSH 트래픽(포트 22)을 허용하는 규칙이 INPUT 체인에 없습니다"
-fi
+    OK "수신 SSH 트래픽(포트 22)을 허용하는 규칙이 INPUT 체인에 있습니다"
+  else
+    WARN "수신 SSH 트래픽(포트 22)을 허용하는 규칙이 INPUT 체인에 없습니다"
+  fi
 
 INFO "특정 규칙에 대한 출력 체인 확인"
 iptables -C OUTPUT -p tcp --dport 80 -j ACCEPT
+
 if [ $? -eq 0 ]; then
-  OK "출력 체인에 발신 HTTP 트래픽(포트 80)을 허용하는 규칙이 있습니다"
-else
-  WARN "출력 체인에 발신 HTTP 트래픽(포트 80)을 허용하는 규칙이 없습니다"
+    OK "출력 체인에 발신 HTTP 트래픽(포트 80)을 허용하는 규칙이 있습니다"
+  else
+    WARN "출력 체인에 발신 HTTP 트래픽(포트 80)을 허용하는 규칙이 없습니다"
+  fi
 fi
  
 
