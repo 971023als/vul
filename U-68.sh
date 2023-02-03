057b5f540afc071772899855ed3518e6483668c9
971023als <971023als@naver.com>
2023-02-03 20:04:18 +0900

    Update U-68.sh

--- U-68.sh
+++ U-68.sh
@@ -24,36 +24,35 @@ EOF
 BAR
 
 
-
-# 서버에 로그온 메시지가 설정되지 않았는지 확인합니다
+## 로그온 메시지가 서버에 대해 설정
 if [ ! -f /etc/motd ]; then
     WARN "로그온 메시지가 서버에 대해 설정되지 않았습니다."
 else
     OK "로그온 메시지가 서버에 대해 설정되어 있습니다."
 fi
 
-# Telnet 서비스에 대해 로그온 메시지가 설정되지 않았는지 확인합니다
+# 로그온 메시지가 텔넷 서비스에 대해 설정
 if [ ! -f /etc/issue.net ]; then
     WARN "로그온 메시지가 텔넷 서비스에 대해 설정되지 않았습니다."
 else
     OK "로그온 메시지가 텔넷 서비스에 대해 설정되어 있습니다."
 fi
 
-# 로그온 메시지가 FTP 서비스에 대해 설정되지 않았는지 확인합니다
-if [ ! -f /etc/ftpbanner ]; then
+# 로그온 메시지가 FTP 서비스에 대해 설정
+if [ ! -f /etc/vsftpd/vsftpd.conf ]; then
     WARN "로그온 메시지가 FTP 서비스에 대해 설정되지 않았습니다."
 else
     OK "로그온 메시지가 FTP 서비스에 대해 설정되어 있습니다."
 fi
 
-# SMTP 서비스에 대해 로그온 메시지가 설정되지 않았는지 확인합니다
-if [ ! -f /etc/postfix/smtpd_banner ]; then
+# 로그온 메시지가 SMTP 서비스에 대해 설정
+if [ ! -f /etc/mail/sendmail.cf ]; then
     WARN "로그온 메시지가 SMTP 서비스에 대해 설정되지 않았습니다."
 else
     OK "로그온 메시지가 SMTP 서비스에 대해 설정되어 있습니다."
 fi
 
-# DNS 서비스에 대해 로그온 메시지가 설정되지 않았는지 확인합니다
+# 로그온 메시지가 DNS 서비스에 대해 설정
 if [ ! -f /etc/named.conf ]; then
     WARN "로그온 메시지가 DNS 서비스에 대해 설정되지 않았습니다."
 else
