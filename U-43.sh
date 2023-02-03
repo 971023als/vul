f4ec5cdf9eeb122f45a81f0f61b4616a99a5bd48
971023als <971023als@naver.com>
2023-02-03 21:14:22 +0900

    Update U-43.sh

--- U-43.sh
+++ U-43.sh
@@ -25,29 +25,49 @@ EOF
 BAR
 
 
-# 로그 파일 경로 설정
-log_file="/var/log/system.log"
+# su 시도에 관한 로그 검토
 
-# 로그 파일이 있는지 확인하십시오
-if [ ! -f $log_file ]; then
-    WARN "시스템 로그 파일을 찾을 수 없습니다"
-else
-    # 로그 파일이 최근 7일 이내에 업데이트되었는지 확인합니다
-    if test $(find $log_file -mtime -7); then
-        OK "시스템 로그 파일이 최근 7일 이내에 업데이트되고 로그 기록이 정기적으로 검토되고 있습니다."
-    else
-        WARN "지난 7일 이내에 시스템 로그 파일이 업데이트되지 않았습니다. 로그 기록이 정기적으로 검토되지 않을 수 있습니다."
-    fi
+# su 명령을 통해 sulog에서 권한 상승 시도 여부를 점검하십시오
+allowed_accounts=("root" "bin" "daemon" "adm" "lp" "sync" "shutdown" "halt" "ubuntu" "user")
 
-    # grep을 사용하여 로그 분석 및 보고가 수행되는지 확인
-    Result=$(grep -E "^[ \t]*Log analysis and reporting performed" $log_file)
+# sulog에서 su 명령을 통해 권한 상승 시도 확인 중
+INFO "sulog에서 su 명령을 통해 권한 상승 시도 확인 중..."
+while read line; do
+  user=$(echo $line | awk '{print $1}')
+  if [[ ! " ${allowed_accounts[@]} " =~ " ${user} " ]]; then
+    WARN "사용자별 권한 상승 시도: $user"
+  else
+    OK "권한 상승 시도가 없습니다."
+  fi
+done < /var/log/sulog
 
-    if [ -n "$Result" ]; then
-        OK "로그 분석 및 보고 수행했습니다."
-    else
-        WARN "로그 분석 및 보고가 수행되지 않을 수 있습니다"
-    fi
-fi
+# 반복적인 로그인 실패에 관한 로그 검토
+
+# 최대 허용 로그인 시도 실패 횟수 정의
+max_failures=5
+
+# 로그에서 반복적인 로그인 실패 여부 확인
+INFO "로그에서 반복적인 로그인 실패를 확인하는 중..."
+failed_logins=$(grep "Failed password" /var/log/auth.log | awk '{print $11}' | sort | uniq -c | awk '{print $1}')
+for failures in $failed_logins; do
+  if [ $failures -gt $max_failures ]; then
+    ip=$(grep "Failed password" /var/log/auth.log | awk '{print $11}' | sort | uniq -c | awk '{if ($1 == "'$failures'") print $2}')
+    WARN "IP 주소에서 반복된 로그인 실패: $ip(로그인 실패 시도: $failures)"
+  else
+    OK "반복된 로그인 실패가 없습니다."
+  fi
+done
+
+
+# 로그인 거부에 관한 로그 검토
+
+# 로그 파일 위치
+LOG_FILE="/var/log/auth.log"
+
+# 로그 파일에서 로그인 거부 메시지 검색
+grep "authentication failure" $LOG_FILE
+
+
 
 cat $result
 
