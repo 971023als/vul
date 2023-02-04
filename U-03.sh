fb765fee080301941d32e749e05536e9676be2c2
971023als <971023als@naver.com>
2023-02-04 12:37:04 +0900

    Update U-03.sh

--- U-03.sh
+++ U-03.sh
@@ -24,16 +24,35 @@ EOF
 
 BAR
 
-# 계정 잠금 임계값이 /etc/pam.d/common-auth에 설정되어 있는지 확인하십시오
-if ! grep -q "auth required pam_tally2.so" /etc/pam.d/common-auth; then
-    WARN "계정 잠금 임계값이 /etc/pam.d/common-auth에서 설정되지 않았습니다."
-else
-    # 계정 잠금 임계값이 10 미만인지 확인하십시오
-    if grep -q "auth required pam_tally2.so deny=10" /etc/pam.d/common-auth; then
-        WARN "계정 잠금 임계값이 10 미만으로 설정됨"
-    fi
+
+#  잠금 임계값 설정
+lock_threshold=5
+
+# pam_taly2 모듈이 설치되어 있는지 확인하십시오
+if ! command -v pam_tally2 >/dev/null 2>&1; then
+  INFO "pam_tally2 모듈이 설치되지 않았습니다. 설치해 주세요."
 fi
-OK "계정 잠금 임계값이 10회 이하의 값으로 설정됩니다."
+
+# /etc/pam.d/common-auth 파일에서 계정 잠금 임계값을 가져옵니다
+current_threshold=$(grep "auth required pam_tally2.so" /etc/pam.d/common-auth | awk '{print $6}' | sed 's/deny=//g')
+
+# 잠금 임계값 비교
+if [ "$lock_threshold" -ne "$current_threshold" ]; then
+  WARN "계정 잠금 임계값이 $lock_threshold로 설정되지 않았습니다. 현재 값은 $current_threshold입니다."
+else
+  OK "계정 잠금 임계값이 $lock_threshold로 설정되었습니다."
+fi
+
+# 계정 잠금 임계값을 가져옵니다
+threshold=$(grep "^auth[[:space:]]*required[[:space:]]*pam_tally2.so.*" /etc/pam.d/common-auth | awk '{print $NF}')
+
+# 임계값이 10보다 작거나 같은지 점검하십시오
+if [[ $threshold -le 10 ]]; then
+  OK "계정 잠금 임계값이 10보다 작거나 같은 $threshold로 설정되었습니다."
+  INFo "제대로 된 보안 설정이 아닙니다. 임계값을 늘리는 것을 고려하십시오."
+else
+  WARN "계정 잠금 임계값이 10보다 큰 $threshold로 설정되었습니다."
+fi
 
 
 
