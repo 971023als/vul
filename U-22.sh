6c5814a9b2ddc22aa4d11f08560bc75c8e4aa118
971023als <971023als@naver.com>
2023-02-04 12:02:27 +0900

    Update U-22.sh

--- U-22.sh
+++ U-22.sh
@@ -24,65 +24,36 @@ EOF
 
 BAR
 
- 
+# 파일 정의
+files=(/etc/crontab /etc/cron.hourly /etc/cron.daily /etc/cron.weekly /etc/cron.monthly /etc/cron.allow /etc/cron.deny /var/spool/cron/ /var/spool/cron/crontabs/)
+
+for file in "${files[@]}"; do
+  if [ -e "$file" ]; then
+    owner=$(stat -c %U "$file")
+    if [ "$owner" != "root" ]; then
+      WARN "$file은 root가 아닌 $owner가 소유합니다"
+    fi
+  else
+    INFO "$file이 존재하지 않습니다"
+  fi
+done
+
+for file in "${files[@]}"; do
+  if [ -e "$file" ]; then
+    perms=$(stat -c %a "$file")
+    if [ "$perms" -lt 640 ]; then
+      WARN "$file에 $perms 권한이 있으며 이 권한은 640보다 작습니다"
+    fi
+  else
+    INFO "$file이 존재하지 않습니다"
+  fi
+done
 
  
 
-# cat filepem.list
-
-# /etc/cron.allow root 640 rw-r-----
-
-# /etc/cron.deny root 640 rw-r-----
-
  
 
- 
 
-CHECK_FILE=/usr/bin/crontab
-
-CHOWN=$(ls -l /usr/bin/crontab | awk '{print $3}')
-
-CHECK_PERM=$(find /usr/bin/crontab -type f -perm -644 -ls | grep -v rw-r--r-- | awk '{print $3}')
-
-CHECK_PERM2=$(ls -l /usr/bin/crontab | awk '{print $1}')
-
- 
-
-if [ -f $CHECK_FILE ] ; then
-
-INFO "$CHECK_FILE 파일이 존재하며 소유자와 권한을 체크합니다."
-
-if [ $CHOWN = 'root' ] ; then
-
-OK "파일의 소유자가 root 입니다."
-
-if [ $CHECK_PERM2 > 640 ] ; then
-
-WARN "파일의 권한이 640 이상으로 되어 있습니다."
-
-echo
-
-echo "$CHECK_FILE 의 권한이 $CHECK_PERM2 으로 되어 있습니다." > $TMP1
-
-INFO "권한 설정 상태는 $TMP1 파일에서 확인하세요."
-
-else
-
-OK "파일의 권한이 640 이하로 되어 있습니다."
-
-fi
-
-else
-
-WARN "파일의 소유자가 root가 아닙니다."
-
-fi
-
-else
-
-INFO "$CHECK_FILE 이 존재하지 않습니다."
-
-fi
 
 cat $result
 
