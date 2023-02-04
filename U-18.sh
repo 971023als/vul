fb5b9e19802f1d4bd52bfe7e691243fc1cd4f696
971023als <971023als@naver.com>
2023-02-04 12:08:32 +0900

    Update U-18.sh

--- U-18.sh
+++ U-18.sh
@@ -24,29 +24,15 @@ EOF
 BAR
 
 
-INFO "iptables 규칙 표시:"
-iptables -S
+CHECK1=$(cat /etc/hosts.allow | grep -v "^#")
+CHECK2=$(cat /etc/hosts.deny | grep -v "^#")
 
-INFO "특정 규칙에 대한 입력 체인 확인"
-iptables -C INPUT -p tcp --dport 22 -j ACCEPT
-if [ $? -eq 0 ]; then
-    OK "수신 SSH 트래픽(포트 22)을 허용하는 규칙이 INPUT 체인에 있습니다"
-  else
-    WARN "수신 SSH 트래픽(포트 22)을 허용하는 규칙이 INPUT 체인에 없습니다"
-  fi
-
-INFO "특정 규칙에 대한 출력 체인 확인"
-iptables -C OUTPUT -p tcp --dport 80 -j ACCEPT
-
-if [ $? -eq 0 ]; then
-    OK "출력 체인에 발신 HTTP 트래픽(포트 80)을 허용하는 규칙이 있습니다"
-  else
-    WARN "출력 체인에 발신 HTTP 트래픽(포트 80)을 허용하는 규칙이 없습니다"
-  fi
+if [ -n "$CHECK1" ] && [ -n "$CHECK2" ] ; then
+	OK "접속을 허용할 특정 호스트에 대한 IP 주소 및 포트 제한을 설정한 경우"	
+else
+	WARN "접속을 허용할 특정 호스트에 대한 IP 주소 및 포트 제한을 설정하지 않은 경우"
 fi
  
-
-
 
  
 
