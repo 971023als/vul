94859c154809b9e1bb16671a9adcdd17ea2f9a8f
971023als <971023als@naver.com>
2023-02-06 13:46:49 +0900

    Update U-21.sh

--- U-21.sh
+++ U-21.sh
@@ -30,22 +30,14 @@ EOF
 
 BAR
 
+rsh_status=$(service is-active rsh.socket)
+rlogin_status=$(service is-active rlogin.socket)
+rexec_status=$(service is-active rexec.socket)
 
-CHECK1=$(ls -alL /etc/xinetd.d/* | egrep 'rsh|rlogin|rexec' | egrep -v 'grep|klogin|kshell|kexec' | wc -l)
-CHECK2=$(ls -alL /etc/xinetd.d/* | egrep 'rsh|rlogin|rexec' | egrep -v 'grep|klogin|kshell|kexec' |awk '{print $NF}' >> /root/linuxs/U-21.txt)
-
-FILECHECK1=$(cat $FILE1 | grep disable | awk '{print $3}')
-FILECHECK2=$(cat $FILE2 | grep disable | awk '{print $3}')
-FILECHECK3=$(cat $FILE3 | grep disable | awk '{print $3}')
-
-if [ $CHECK1 != 0 ] ; then
-	if [ $FILECHECK1 == 'yes' ] && [ $FILECHECK2 == 'yes' ] && [ $FILECHECK3 == 'yes' ] ; then
-		OK "불필요한 r 계열 서비스가 비활성화 되어 있는 경우"
-	else
-		WARN "불필요한 r 계열 서비스가 활성화 되어 있는 경우"
-	fi
+if [ "$rsh_status" == "active" ] && [ "$rlogin_status" == "active" ] && [ "$rexec_status" == "active" ]; then
+  WARN "rsh, rlogin 및 exec 서비스가 실행 중"
 else
-	OK "불필요한 r 계열 서비스가 비활성화 되어 있는 경우"
+  OK "하나 이상의 rsh, rlogin 및 exec 서비스가 실행되고 있지 않습니다."
 fi
 
 cat $result
