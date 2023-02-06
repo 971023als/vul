0daa09e49e2dbc9b62b69016ad90786e48053d35
971023als <971023als@naver.com>
2023-02-06 15:55:33 +0900

    Update U-34.sh

--- U-34.sh
+++ U-34.sh
@@ -24,29 +24,7 @@ EOF
 
 BAR
 
-# dig 명령이 설치되어 있는지 확인하십시오
-if ! command -v dig >/dev/null 2>&1; then
-    INFO "'dig' 명령이 설치되지 않았습니다. 계속하기 전에 설치하십시오."
-fi
 
-# 로컬 해결사가 작동 중인지 확인하십시오
-INFO "로컬 확인 중..."
-if dig +short localhost >/dev/null 2>&1; then
-    WARN "로컬 해결사가 작동 중입니다."
-else
-    OK "로컬 해결사가 작동하지 않습니다. DNS 구성을 확인하십시오."
-fi
-
-# 공용 DNS 서버가 작동 중인지 확인
-INFO "공용 DNS 서버 확인 중..."
-if dig +short google.com >/dev/null 2>&1; then
-    WARN "공용 DNS 서버가 작동 중입니다."
-else
-    OK "공용 DNS 서버가 작동하지 않습니다. 인터넷 연결 또는 DNS 구성을 확인하십시오."
-fi
-
-# 모든 검사 통과
-INFO "DNS 서비스가 예상대로 작동하고 있습니다."
 
 
 
