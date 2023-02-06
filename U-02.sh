6bf43cf2e0d89af9d5aead5195ad7572bc5c161d
971023als <971023als@naver.com>
2023-02-06 11:00:23 +0900

    Update U-02.sh

--- U-02.sh
+++ U-02.sh
@@ -28,7 +28,7 @@ BAR
 
 # /etc/login.defs에서 최소 암호 길이 확인
 min_len_defs=$(grep "^PASS_MIN_LEN" /etc/login.defs | awk '{print $2}')
-if [ "$min_len_defs" -ge 8 ]; then
+if [ "$(expr "$min_len_defs" + 0)" -ge 8 ]; then
   OK "/etc/login.defs의 최소 암호 길이가 $min_len_defs 로 설정됨"
 else
   WARN "/etc/login.defs의 최소 암호 길이가 8보다 작음"
@@ -36,7 +36,7 @@ fi
 
 # /etc/pam.d/system-auth에서 영어, 숫자 및 특수 문자 설정 확인
 min_len_pam=$(grep "pam_cracklib.so" /etc/pam.d/system-auth | grep "minlen" | awk -F"=" '{print $2}')
-if [ "$min_len_pam" -ge 8 ]; then
+if [ "$(expr "$min_len_pam" + 0)" -ge 8 ]; then
   OK "/etc/pam.d/system-auth의 최소 암호 길이가 $min_len_pam으로 설정됨"
 else
   WARN "/etc/pam.d/system-auth의 최소 암호 길이가 8보다 작습니다."
