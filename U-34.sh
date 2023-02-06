0c71648306c69a971fbacf42616b8bc90997b0fd
971023als <971023als@naver.com>
2023-02-06 15:57:12 +0900

    Update U-34.sh

--- U-34.sh
+++ U-34.sh
@@ -24,6 +24,15 @@ EOF
 
 BAR
 
+# DNS 프로세스가 지금 실행 중인지 확인합니다
+dns_process=$(ps -ef | grep named | grep -v grep)
+
+# DNS 프로세스가 계속 실행되면 오류 메시지를 인쇄합니다
+if [ -z "$dns_process" ]; then
+  OK "DNS 서비스 데몬이 실행되고 있지 않습니다."
+else
+  WARN "DNS 서비스 데몬이 실행 중입니다."
+fi
 
 
 
