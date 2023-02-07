689a252b08aeb88a53b0c051150d6eae105b12f6
971023als <971023als@naver.com>
2023-02-07 21:41:07 +0900

    Update U-38.sh

--- U-38.sh
+++ U-38.sh
@@ -28,12 +28,14 @@ HTTPD_ROOT="/etc/apache2/apache2.conf"
 UNWANTED_ITEMS="manual samples docs"
 
 if [ `ps -ef | grep httpd | grep -v "grep" | wc -l` -eq 0 ]; then
-    echo "Apache is not running."
+    INFO "아파치가 실행되지 않습니다."
 else
     for item in $UNWANTED_ITEMS
     do
         if [ ! -d "$HTTPD_ROOT/$item" ] && [ ! -f "$HTTPD_ROOT/$item" ]; then
-            echo "$item not found in $HTTPD_ROOT"
+            OK "$item 을 $HTTPD_ROOT 에서 찾을 수 없습니다"
+        else
+            WARN "$item 을 $HTTPD_ROOT 에서 찾을 수 있습니다"
         fi
     done
 fi
