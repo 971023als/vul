3d3ea6ef824303f5c2cc1dc0436bdba29be486dd
971023als <971023als@naver.com>
2023-02-08 22:17:03 +0900

    Update U-51.sh

--- U-51.sh
+++ U-51.sh
@@ -36,13 +36,25 @@ declare -a necessary_groups=("root" "sudo" "sys" "adm" "wheel"
 "apache" "postfix" "gdm" "adiosl" "mysql" "cubrid"
  "messagebus" "syslog" "avahi" "whoopsie"
 "colord" "systemd-network" "systemd-resolve"
-"systemd-timesync" "mysql" "sync" "user")
+"systemd-timesync" "mysql" "sync" "user"
+"tty" "disk" "men" "kmen" "mail" "uucp"
+"man" "games" "gopher" "video" "dip"
+"ftp" "lock" "audio" "nobody" "users"
+"usbmuxd" "utmp" "utempter" "rtkit"
+"avahi-autoipd" "desktop_admin_r"
+"desktop_user_r" "floppy"
+"vcsa" "abrt" "cdrom" "tape"
+"dialout" "wbpriv" "nfsnonody"
+"ntp" "saslauth" "postdrop"
+"pulse" "pulse-access" "fuse" 
+"sshd" "slocate" "stapusr"
+"stapsys" "tcpdump" "named")
 
 all_groups=$(getent group | cut -d: -f1)
 
 for group in $all_groups; do
   if ! [[ " ${necessary_groups[@]} " =~ " ${group} " ]]; then
-    INFO "Group ${group}은(는) 시스템 관리 또는 운영에 필요하지 않으므로 검토해야 합니다."
+    WARN "Group ${group}은(는) 시스템 관리 또는 운영에 필요하지 않으므로 검토해야 합니다."
   else
     OK "Group ${group}은(는) 시스템 관리 또는 운영에 필요합니다."
   fi
