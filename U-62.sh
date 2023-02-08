c899695952738b616c73c3da7b09a45b0a6f6d8c
971023als <971023als@naver.com>
2023-02-08 22:54:58 +0900

    Update U-62.sh

--- U-62.sh
+++ U-62.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
- 
-
 . function.sh
-
  
 TMP1=`SCRIPTNAME`.log
 
 > $TMP1 
  
-
 BAR
 
 CODE [U-62] ftp 계정 shell 제한
@@ -27,17 +23,13 @@ BAR
 # /etc/passwd 파일에서 FTP 계정의 항목을 가져옵니다
 ftp_entry=$(grep "^ftp:" /etc/passwd)
 
-# /etc/passwd 파일에서 FTP 계정의 항목을 가져옵니다
-ftp_shell=$(echo $ftp_entry | awk -F: '{print $7}')
-
-# FTP 계정의 셸을 /bin/false와 비교합니다
+# FTP 계정의 셸을 확인하여 변경 사항 확인
+ftp_shell=$(grep "^ftp:" /etc/passwd | awk -F: '{print $7}')
 if [ "$ftp_shell" == "/bin/false" ]; then
-  OK "FTP 계정이 /bin/false 셸이 부여되었습니다."
+  OK "FTP 계정의 셸이 /bin/false로 설정되었습니다."
 else
-  WARN "FTP 계정이 /bin/false 셸이 부여되지 않았습니다."
+  INFO "FTP 계정의 셸을 /bin/false로 설정할 수 없습니다."
 fi
-
-
 
 cat $result
 
