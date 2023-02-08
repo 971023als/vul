e870156c59fd202c8e180428f65500996c7cf6dd
971023als <971023als@naver.com>
2023-02-08 22:02:50 +0900

    Update U-45.sh

--- U-45.sh
+++ U-45.sh
@@ -21,32 +21,41 @@ EOF
 
 BAR
 
-# 휠 그룹이 /etc/group 파일에 정의되어 있는지 확인하십시오
-grep -q '^wheel:' /etc/group
-if [ $? -eq 0 ]; then
-  # 휠 그룹이 정의됨
-  
-  # SUID 비트가 su 명령에 대해 설정되었는지 확인합니다
-  ls -l $(which su) | grep -q '^-rwsr-xr-x'
-  if [ $? -eq 0 ]; then
-    # SUID 비트가 설정되었습니다
-    
-    # 휠 그룹이 su 실행 권한이 있는 유일한 그룹인지 점검하십시오
-    ls -l $(which su) | grep -q '^.*wheel.*$'
-    if [ $? -eq 0 ]; then
-      # 휠 그룹에만 su 실행 권한이 있습니다
-      OK "SU 명령은 휠 그룹의 사용자로 제한됩니다."
-    else
-      # 허가는 휠 그룹으로만 제한되지 않습니다
-      WARN "SU 명령은 휠 그룹의 사용자로 제한되지 않습니다."
-    fi
-  else
-    # SUID 비트가 SU 명령에 대해 설정
-    WARN "SUID 비트가 SU 명령에 대해 설정되지 않았습니다."
-  fi
+
+# 휠 그룹이 존재하는지 점검하십시오
+if ! grep -q "^wheel:" /etc/group; then
+  WARN "휠 그룹이 존재하지 않습니다."
 else
-  # 휠 그룹이 /etc/group 파일에 정의
-  WARN "휠 그룹이 /etc/group 파일에 정의되어 있지 않습니다."
+  OK "휠 그룹이 존재합니다."
+fi
+
+# su 명령이 휠 그룹에 의해 소유되는지 점검하십시오
+if ! [ $(stat -c %G /bin/su) == "wheel" ]; then
+  WARN "su 명령은 휠 그룹이 소유하지 않습니다."
+else
+  OK "휠 그룹이 존재합니다."
+fi
+
+# su 명령에 권한 4750이 있는지 확인하십시오
+if ! [ $(stat -c %a /bin/su) == "4750" ]; then
+  WARN "su 명령에 올바른 권한이 없습니다."
+else
+  OK "휠 그룹이 존재합니다."
+fi
+
+# 휠 그룹에 su 명령을 사용할 수 있는 계정이 있는지 확인하십시오
+found=false
+for user in $(grep "^wheel:" /etc/group | cut -d ":" -f4 | tr "," "\n"); do
+  if id -nG "$user" | grep -qw "wheel"; then
+    found=true
+    break
+  fi
+done
+
+if ! $found; then
+  WARN "휠 그룹의 어떤 계정도 su 명령을 사용할 수 없습니다."
+else
+  OK "휠 그룹의 어떤 계정도 su 명령을 사용할 수 있습니다."
 fi
  
 
