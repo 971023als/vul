a3651e2ccd8025d164647f60e791ee49252b5d91
971023als <971023als@naver.com>
2023-02-08 14:59:37 +0900

    Update U-43.sh

--- U-43.sh
+++ U-43.sh
@@ -24,10 +24,51 @@ EOF
 
 BAR
 
-# su 시도에 관한 로그 검토
+# 로그 파일의 경로 정의
+LOG_DIR="/var/log"
+UTMP="$LOG_DIR/utmp"
+WTMP="$LOG_DIR/wtmp"
+BTMP="$LOG_DIR/btmp"
 
-# su 명령을 통해 sulog에서 권한 상승 시도 여부를 점검하십시오
-allowed_accounts=(
+# 로그 파일이 있는지 확인합니다
+if [ ! -f "$UTMP" ]; then
+  WARN "$UTMP 가 없습니다"
+else
+  OK "$UTMP 가 있습니다"
+fi
+
+if [ ! -f "$WTMP" ]; then
+  WARN "$WTMP 가 없습니다"
+else
+  OK "$WTMP 가 있습니다"
+fi
+
+if [ ! -f "$BTMP" ]; then
+  WARN "$BTMP 가 없습니다"
+else
+  OK "$BTMP 가 있습니다"
+fi
+
+# '마지막' 명령을 사용하여 로그 정보를 표시
+last -f "$WTMP"
+
+# 'failog' 명령을 사용하여 실패한 로그인 시도를 표시합니다
+faillog
+
+# 해킹 시도 증거 'btmp' 파일 내용 확인
+cat "$BTMP" | awk '{print $1, $2, $3, $4, $5}'
+
+# 추가 분석을 위해 결과를 파일로 출력
+echo "Last login information:" > log_review.txt
+last -f "$WTMP" >> log_review.txt
+echo "Failed login attempts:" >> log_review.txt
+faillog >> log_review.txt
+echo "Brute force login attempts:" >> log_review.txt
+cat "$BTMP" | awk '{print $1, $2, $3, $4, $5}' >> log_review.txt
+
+
+SULOG_FILE="/var/log/sulog"
+ALLOWED_ACCOUNTS=(
   "root"
   "bin"
   "daemon"
@@ -59,52 +100,46 @@ allowed_accounts=(
   "cubrid"
 )
 
-
-# sulog에서 su 명령을 통해 권한 상승 시도 확인 중
-
-INFO "sulog에서 su 명령을 통해 권한 상승 시도 확인 중..."
-if [ ! -f /var/log/sulog ]; then
-  INFO "/var/log/sulog 파일을 찾을 수 없습니다"
-else
-  while read line; do
-    user=$(echo $line | awk '{print $1}')
-    if [[ ! " ${allowed_accounts[@]} " =~ " ${user} " ]]; then
-      INFO "사용자별 권한 상승 시도: $user"
-    else
-      OK "권한 상승 시도가 없습니다."
-    fi
-  done < /var/log/sulog
-fi
-
-# 반복적인 로그인 실패에 관한 로그 검토
-
-# 반복적인 로그인 실패에 대한 임계값 설정
-threshold=5
-
-# 로그 파일에서 반복되는 로그인 실패 횟수를 가져옵니다
-count=$(grep -c "Failed password" /var/log/auth.log)
-
-# 로그에서 반복적인 로그인 실패 여부 확인
-INFO "로그에서 반복적인 로그인 실패를 확인하는 중..."
-if [ ! -f /var/log/auth.log ]; then
-  INFO "/var/log/auth.log 파일을 찾을 수 없습니다"
-else
-  if [ "$count" -gt "$threshold" ]; then
-    WARN "반복적인 로그인 실패가 탐지되었습니다. 총 실패 수: $count"
-  else
-    OK "로그인 실패가 반복되지 않습니다. 총 실패 수: $count"
+INFO "$SULOG_FILE 의 su 명령을 통해 확인 권한이 증가합니다"
+while read line; do
+  username=$(echo $line | awk '{print $1}')
+  granted=$(echo $line | awk '{print $3}')
+  if [ "$granted" != "to" ]; then
+    continue
   fi
+
+  granted_to=$(echo $line | awk '{print $4}')
+  if [[ $ALLOWED_ACCOUNTS =~ (^|[[:space:]])"$granted_to"($|[[:space:]]) ]]; then
+    OK "권한을 $granted_to 로 $username 만큼 증가할 수 있습니다."
+  else
+    WARN "권한을 $granted_to 로 $username 은(는) 허용되지 않습니다."
+  fi
+done < $SULOG_FILE
+
+XFERLOG="/var/log/xferlog"
+
+# xferlog 파일이 있는지 확인합니다
+if [ -f $XFERLOG ]; then
+  # awk를 사용하여 xferlog 파일의 각 라인에 대한 IP 주소, 로그인 이름 및 액세스 날짜를 인쇄합니다
+  awk '{print $9 " " $10 " " $1}' $XFERLOG | while read line
+  do
+    # 각 라인에서 IP 주소, 로그인 이름 및 액세스 날짜 추출
+    IP=$(echo $line | awk '{print $1}')
+    USER=$(echo $line | awk '{print $2}')
+    DATE=$(echo $line | awk '{print $3}')
+  
+    # 로그인 이름이 허용된 계정 목록에 없는지 확인합니다
+    if ! grep -q $USER /etc/ftpusers; then
+      # 무단 FTP 액세스에 대한 경고 메시지 인쇄
+      WARN "$DATE 의 사용자 $USER 에 대한 IP $IP 에서 인증되지 않은 FTP 액세스가 탐지됨"
+    else
+      OK "$DATE 의 사용자 $USER 에 대한 IP $IP 에서 인증되지 않은 FTP 액세스가 탐지 안 됨"
+    fi
+  done
+else
+  # xferlog 파일이 없는 경우 오류 메시지 인쇄
+  WARN "xferlog 파일 $XFERLOG를 찾을 수 없습니다"
 fi
-
-
-# 로그인 거부에 관한 로그 검토
-
-# 로그 파일 위치
-LOG_FILE="/var/log/auth.log"
-
-# 로그 파일에서 로그인 거부 메시지 검색
-grep "authentication failure" $LOG_FILE
-
 
 
 
