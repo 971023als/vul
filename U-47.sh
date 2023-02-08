33cff5eb602e09f1059f83c53c31abd0d9c9b289
971023als <971023als@naver.com>
2023-02-08 14:32:47 +0900

    Update U-47.sh

--- U-47.sh
+++ U-47.sh
@@ -23,14 +23,18 @@ EOF
 
 BAR
 
-# PASS_MAX_DA의 값을 가져옵니다
-pass_max_days=$(grep "^PASS_MAX_DAYS" /etc/login.defs | awk '{print $2}')
+# login.defs 파일에서 PASS_MAX_DAYS 값을 가져옵니다
+pass_max_days=$(grep -E "^PASS_MAX_DAYS" /etc/login.defs | awk '{print $2}')
 
 # 값이 90보다 작거나 같은지 확인합니다
-if [ $pass_max_days -le 90 ]; then
-  OK "PASS_MAX_DAYS가 90 이하인 $pass_max_days 로 설정되었습니다."
+if grep -q "^PASS_MAX_DAYS" /etc/login.defs; then
+  WARN "PASS_MAX_DAYS이 주석 처리되었습니다."
 else
-  WARN "PASS_MAX_DAYS가 90보다 큰 $pass_max_days 로 설정되었습니다."
+  if [ $pass_max_days -le 90 ]; then
+    OK "PASS_MAX_DAYS가 90 이하인 $pass_max_days 로 설정되었습니다."
+  else
+    WARN "PASS_MAX_DAYS가 90보다 큰 $pass_max_days 로 설정되었습니다."
+  fi
 fi
 
  
