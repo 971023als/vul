2495f200882b73e383e0cc8a218fc098fbdcd994
971023als <971023als@naver.com>
2023-02-08 23:04:06 +0900

    Update U-57.sh

--- U-57.sh
+++ U-57.sh
@@ -2,7 +2,6 @@
 
 . function.sh
  
-
 BAR
 
 CODE [U-57] 홈 디렉터리 소유자 및 권한
@@ -17,31 +16,36 @@ EOF
 
 BAR
 
-# /etc/passwd의 각 줄 읽기
-while read line; do
-  # 구분 기호로 ':'를 사용하여 줄을 필드로 분할
-  fields=($(echo $line | tr ':' ' '))
-  username=${fields[0]}
-  home_dir=${fields[5]}
-  owner=$(ls -ld "$home_dir" | awk '{print $3}')
+# 구분 기호로 ':'를 사용하여 줄을 필드로 분할
+fields=($(echo $line | tr ':' ' '))
+username=${fields[0]}
+home_dir=${fields[5]}
+owner=$(ls -ld "$home_dir" | awk '{print $3}')
 
-  # 홈 디렉토리 소유자가 사용자 이름과 일치하는지 확인합니다
-  if [ "$owner" != "$username" ]; then
-    WARN "$home_dir $owner 가 $username 과 일치하지 않습니다"
-  else
-    OK "$home_dir $owner 가 $username 과 일치합니다"
-  fi
-done < /etc/passwd
-
-# 다른 사용자에 대한 쓰기 권한 확인
+# 다른 사용자에 대한 쓰기 권한 변수 설정
 permissions=$(stat -c "%a" "$home_dir")
 other_write=${permissions:2:1}
-if [ "$other_write" == "w" ]; then
-  WARN "다른 사용자에게 $home_dir 에 대한 쓰기 권한이 있습니다."
-else
-  OK "다른 사용자에게 $home_dir 에 대한 쓰기 권한이 없습니다."
-fi
 
+if [ ! -f "$home_dir" ]; then
+  INFO "$home_dir 가 없습니다."
+else
+  # /etc/passwd의 각 줄 읽기
+  while read line; do
+    # 홈 디렉토리 소유자가 사용자 이름과 일치하는지 확인합니다
+    if [ "$owner" != "$username" ]; then
+      WARN "$home_dir $owner 가 $username 과 일치하지 않습니다"
+    else
+      OK "$home_dir $owner 가 $username 과 일치합니다"
+    fi
+  done < /etc/passwd
+
+  # 다른 사용자에 대한 쓰기 권한 확인
+  if [ "$other_write" == "w" ]; then
+    WARN "다른 사용자에게 $home_dir 에 대한 쓰기 권한이 있습니다."
+  else
+    OK "다른 사용자에게 $home_dir 에 대한 쓰기 권한이 없습니다."
+  fi
+fi
 
 cat $result
 
