2657903475d923b67c1a383590b58d3fe66b4a26
971023als <971023als@naver.com>
2023-02-11 23:19:31 +0900

    Update U-61.sh

--- U-61.sh
+++ U-61.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
- 
-
 . function.sh
 
 TMP1=`SCRIPTNAME`.log
 
 > $TMP1   
-
- 
 
 BAR
 
@@ -32,9 +28,26 @@ if ps -ef | grep -q 'ftp'; then
   WARN "FTP 서비스가 실행 중입니다."
 else
   OK "FTP 서비스가 실행되고 있지 않습니다."
-  INFO "서비스 상태: $ftp_status"
 fi
 
+# FTP 포트가 수신 중인지 확인합니다
+if netstat -tnlp | grep -q ':21'; then
+  WARN "FTP 포트(21)가 열려 있습니다."
+else
+  OK "FTP 포트(21)가 열려 있지 않습니다."
+fi
+
+# /etc/passwd에서 FTP 계정을 확인합니다
+ftp_entry=$(grep "^ftp:" /etc/passwd)
+
+# FTP 계정의 셸을 확인합니다
+ftp_shell=$(grep "^ftp:" /etc/passwd | awk -F: '{print $7}')
+
+if [ "$ftp_shell" == "/bin/false" ]; then
+  WARN "FTP 계정(ftp)이 로그인할 수 없습니다."
+else
+  OK "FTP 계정(ftp)이 로그인할 수 있습니다."
+fi
 
 cat $result
 
