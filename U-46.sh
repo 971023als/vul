ac1541449297115ba5c4464845c9c0e827ccb2fe
971023als <971023als@naver.com>
2023-02-12 02:25:14 +0900

    ok

--- U-46.sh
+++ U-46.sh
@@ -31,18 +31,18 @@ pass_min_len=$(grep -E "^PASS_MIN_LEN" /etc/login.defs | awk '{print $2}')
 
 pass=8
 
-# PASS_MIN_LEN 값이 코멘트 아웃되었는지 확인합니다
+# PASS_MIN_LEN 값이 주석 처리되었는지 확인합니다
 if grep -q "^#PASS_MIN_LEN" /etc/login.defs; then
-  INFO "PASS_MIN_LEN이 주석 처리되었습니다."
+  INFO "PASS_MIN_LEN가 주석 처리되었습니다."
 else
-  # PASS_MIN_LEN의 값이 올바른 정수인지 확인하십시오
+  # PASS_MIN_LENS 값이 올바른 정수인지 확인하십시오
   if [ "$pass_min_len" -eq "$pass_min_len" ] 2>/dev/null; then
     # PASS_MIN_LEN의 값이 지정된 범위 내에 있는지 확인합니다
     if [ "$pass_min_len" -ge 0 ] && [ "$pass_min_len" -le 99999999 ]; then
       if [ "$pass_min_len" -ge "$pass" ]; then
-        WARN "PASS_MIN_LEN이 $pass_min_len으로 설정되어 $pass보다 크거나 같습니다."
+        OK "PASS_MIN_LEN이 $pass_min_len 으로 설정되어 $pass 보다 크거나 같습니다."
       else
-        OK "PASS_MIN_LEN이 $pass보다 작은 $pass_min_len으로 설정되었습니다."
+        WARN "PASS_MIN_LEN이 $pass 보다 작은 $pass_min_len 으로 설정되었습니다."
       fi
     else
       INFO " PASS_MIN_LEN 값이 범위를 벗어났습니다."
--- U-48.sh
+++ U-48.sh
@@ -29,19 +29,26 @@ pass_min_days=$(grep "^PASS_MIN_DAYS" /etc/login.defs | awk '{print $2}')
 
 min_days=7
 
+# PASS_MIN_DAYS 값이 주석 처리되었는지 확인합니다
 if grep -q "^#PASS_MIN_DAYS" /etc/login.defs; then
-  WARN "PASS_MIN_DAYS이 주석 처리되었습니다."
-  # PASS_MIN_DAYS 값이 7 이상인지 확인합니다
-  if [ "$pass_min_len" -ge 0 ] && [ "$pass_min_len" -le 99999999 ]; then
-    if [ "$pass_min_days" -ge "$min_days" ]; then
-      OK "PASS_MIN_DAYS 가 /etc/login.defs에 있습니다."
+  INFO "PASS_MIN_DAYS가 주석 처리되었습니다."
+else
+  # PASS_MIN_DAYS 값이 올바른 정수인지 확인하십시오
+  if [ "$pass_min_days" -eq "$pass_min_days" ] 2>/dev/null; then
+    # PASS_MIN_DAYS의 값이 지정된 범위 내에 있는지 확인합니다
+    if [ "$pass_min_days" -ge 0 ] && [ "$pass_min_days" -le 99999999 ]; then
+      if [ "$pass_min_days" -ge "$min_days" ]; then
+        OK "PASS_MIN_DAYS이 $pass_min_days 으로 설정되어 $min_days 보다 크거나 같습니다."
+      else
+        WARN "PASS_MIN_DAYS이 $min_days 보다 작은 $pass_min_days 으로 설정되었습니다."
+      fi
     else
-      WARN "PASS_MIN_DAYS 가 /etc/login.defs에 없습니다."
+      INFO " PASS_MIN_DAYS 값이 범위를 벗어났습니다."
     fi
   else
-    INFO "PASS_MIN_DAYS 값이 숫자가 아닙니다."
+    INFO " PASS_MIN_DAYS 값이 올바른 정수가 아닙니다."
   fi
-fi 
+fi
 
  
 
