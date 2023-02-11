5291377c00a776d6f18ca3a5241fc8711a600974
971023als <971023als@naver.com>
2023-02-12 04:03:39 +0900

    Update U-02.sh

--- U-02.sh
+++ U-02.sh
@@ -29,12 +29,18 @@ min=8
 if grep -q "^#PASS_MIN_LEN" /etc/login.defs; then
   INFO "PASS_MIN_LEN가 주석 처리되었습니다."
 else
-    if [ "$pass_min_len" -le "$min" ]; then
-      WARN "8 글자 미만의 패스워드가 설정."
+    # PASS_MIN_LEN의 값이 지정된 범위 내에 있는지 확인합니다
+    if [ "$pass_min_len" -ge 0 ] && [ "$pass_min_len" -le 99999999 ]; then
+      if [ "$pass_min_len" -le "$min" ]; then
+        WARN "PASS_MAX_DAYS가 $min 보다 작거나 같은 $pass_min_len 로 설정되었습니다."
+      else
+        OK "PASS_MAX_DAYS가 $min 보다 큰 $pass_min_len 로 설정되었습니다."
+      fi
     else
-      OK "8 글자 이상의 패스워드가 설정."
+      INFO "PASS_MIN_LEN 값이 범위를 벗어났습니다."
     fi
 fi
+
 
 
 PAM_FILE="/etc/pam.d/common-auth"
