0e4bca91d8972f19e2e3f8d542b9d5400718317e
971023als <971023als@naver.com>
2023-02-12 03:44:11 +0900

    Update U-02.sh

--- U-02.sh
+++ U-02.sh
@@ -20,22 +20,29 @@ TMP1=`SCRIPTNAME`.log
 
 >$TMP1 
 
-LOGIN_DEFS_FILE="/etc/login.defs"
-PASS_MIN_LEN_OPTION="PASS_MIN_LEN"
+# login.defs 파일에서 PASS_MIN_LEN 값을 가져옵니다
+pass_min_len=$(grep -E "^PASS_MIN_LEN" /etc/login.defs | awk '{print $2}')
+min=8
 
-if [ ! -f "$LOGIN_DEFS_FILE" ]; then
-    WARN "$LOGIN_DEFS_FILE 을 찾을 수 없습니다."
+# PASS_MIN_LEN 값이 주석 처리되었는지 확인합니다
+if grep -q "^#PASS_MIN_LEN" /etc/login.defs; then
+  INFO "PASS_MIN_LEN가 주석 처리되었습니다."
 else
-    PASS_MIN_LEN=$(grep "^$PASS_MIN_LEN_OPTION" "$LOGIN_DEFS_FILE" | awk '{print $2}')
-    if ! [[ $PASS_MIN_LEN =~ ^[0-9999999]+$ ]]; then
-        INFO "PASS_MAX_DAYS 값이 숫자가 아닙니다."
+  # PASS_MIN_LEN 값이 올바른 정수인지 확인하십시오
+  if [ "$pass_min_len" -eq "$pass_min_len" ] 2>/dev/null; then
+    # PASS_MIN_LEN의 값이 지정된 범위 내에 있는지 확인합니다
+    if [ "$pass_min_len" -ge 0 ] && [ "$pass_min_len" -le 99999999 ]; then
+      if [ "$pass_min_len" -le "$min" ]; then
+        WARN "PASS_MAX_DAYS가 $min 보다 작거나 같은 $pass_min_len 로 설정되었습니다."
+      else
+        OK "PASS_MAX_DAYS가 $min 보다 큰 $pass_min_len 로 설정되었습니다."
+      fi
     else
-        if [ "$PASS_MIN_LEN" -lt 8 ]; then
-            WARN "$LOGIN_DEFS_FILE 에서 $PASS_MIN_LEN_OPTION 값이 8보다 작습니다."
-        else
-            OK "$LOGIN_DEFS_FILE 에서 $PASS_MIN_LEN_OPTION 값이 8 이상입니다."
-        fi
+      INFO "PASS_MIN_LEN 값이 범위를 벗어났습니다."
     fi
+  else
+    INFO "PASS_MIN_LEN 값이 올바른 정수가 아닙니다."
+  fi
 fi
 
 PAM_FILE="/etc/pam.d/common-auth"
