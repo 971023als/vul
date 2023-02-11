4e6ef5431f76da83adbd16ad2978dfefc83c4622
971023als <971023als@naver.com>
2023-02-12 00:01:04 +0900

    Update U-61.sh

--- U-61.sh
+++ U-61.sh
@@ -23,13 +23,6 @@ BAR
 # FTP 서비스의 상태를 확인합니다
 ftp_status=$(service ftp status 2>&1)
 
-# FTP 서비스가 실행 중인지 확인합니다
-if ps -ef | grep -q 'ftp'; then
-  WARN "FTP 서비스가 실행 중입니다."
-else
-  OK "FTP 서비스가 실행되고 있지 않습니다."
-fi
-
 # FTP 포트가 수신 중인지 확인합니다
 if netstat -tnlp | grep -q ':21'; then
   WARN "FTP 포트(21)가 열려 있습니다."
