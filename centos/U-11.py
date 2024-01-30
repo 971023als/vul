ccf32db706a65faf62c1b252113633f0eb746345
971023als <971023als@naver.com>
2024-01-30 21:39:22 +0900

    11번

--- centos/U-11.py
+++ centos/U-11.py
@@ -1,54 +1,50 @@
-#!/bin/bash
+#!/usr/bin/env python3
+import json
+import os
+import stat
 
-. function.sh
+# 결과를 저장할 딕셔너리
+results = {
+    "U-11": {
+        "title": "/etc/rsyslog.conf 파일 소유자 및 권한 설정",
+        "status": "",
+        "description": {
+            "good": "/etc/rsyslog.conf 파일의 소유자가 root(또는 bin, sys)이고, 권한이 640 이하인 경우",
+            "bad": "/etc/rsyslog.conf 파일의 소유자가 root(또는 bin, sys)가 아니거나, 권한이 640 이하가 아닌 경우"
+        },
+        "details": []
+    }
+}
 
-TMP1=`SCRIPTNAME`.log
+def check_rsyslog_conf_ownership_and_permissions():
+    rsyslog_conf_file = "/etc/rsyslog.conf"
+    
+    if os.path.exists(rsyslog_conf_file):
+        file_stat = os.stat(rsyslog_conf_file)
+        owner_uid = file_stat.st_uid
+        permissions = stat.S_IMODE(file_stat.st_mode)
+        owner = os.popen(f'stat -c "%U" {rsyslog_conf_file}').read().strip()
 
->$TMP1 
+        if owner not in ["root", "bin", "sys"]:
+            results["U-11"]["status"] = "취약"
+            results["U-11"]["details"].append(f"{rsyslog_conf_file}가 {owner}에 의해 소유되어 있습니다.")
+        else:
+            if permissions <= 0o640:
+                results["U-11"]["details"].append(f"{rsyslog_conf_file}에 대한 사용 권한이 {permissions}으로 안전합니다.")
+                results["U-11"]["status"] = "양호"
+            else:
+                results["U-11"]["status"] = "취약"
+                results["U-11"]["details"].append(f"{rsyslog_conf_file}에 대한 사용 권한이 {permissions}으로 안전하지 않습니다.")
+    else:
+        results["U-11"]["details"].append(f"{rsyslog_conf_file} 존재하지 않습니다.")
+        results["U-11"]["status"] = "정보"
 
+check_rsyslog_conf_ownership_and_permissions()
 
-BAR
+# 결과 파일에 JSON 형태로 저장
+result_file = 'rsyslog_conf_ownership_and_permissions_check_result.json'
+with open(result_file, 'w') as file:
+    json.dump(results, file, indent=4, ensure_ascii=False)
 
-CODE [U-11] /etc/rsyslog.conf 파일 소유자 및 권한 설정 
-
-cat << EOF >> $result 
-
-[양호]: /etc/rsyslog.conf 파일의 소유자가 root(또는 bin, sys)이고, 권한이 640 이하인 경우
-
-[취약]: /etc/rsyslog.conf 파일의 소유자가 root(또는 bin, sys)가 아니거나, 권한이  640 이하가 아닌 경우
-
-EOF
-
-BAR
-
-
-# 파일 소유권 확인
-if [ -e "/etc/rsyslog.conf" ]; then
-  file_owner=$(stat -c %U /etc/rsyslog.conf)
-if [[ "$file_owner" != "root" && "$file_owner" != "bin" && "$file_owner" != "sys" ]]; then
-  WARN " /etc/rsyslog.conf가 루트(또는 bin, sys)에 의해 소유되지 않습니다."
-fi
-
-# 파일 권한 확인
-file_perms=$(stat -c %a /etc/rsyslog.conf)
-dec_perms=$(printf "%d" $file_perms)
-
-if [ $dec_perms -lt 640 ]; then
-      WARN "/etc/rsyslog.conf에 대한 사용 권한은 안전하지 않습니다"
-  else
-      OK "/etc/rsyslog.conf에 대한 사용 권한은 안전합니다"
-  fi
-
-else
-  OK "/etc/rsyslog.conf 존재하지 않음"
-fi
-
-cat $result
-
-echo ; echo
-
- 
-
-
-
- 
+# 결과 콘솔에 출력
+print(json.dumps(results, indent=4, ensure_ascii=False))
