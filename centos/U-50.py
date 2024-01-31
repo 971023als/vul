169b9f5c7d0f8d8f3521a0ceece1177fad5061c4
971023als <971023als@naver.com>
2024-01-31 09:25:17 +0900

    50 51번 수정

--- centos/U-50.py
+++ centos/U-50.py
@@ -1,38 +1,49 @@
-#!/bin/bash
+#!/usr/bin/env python3
+import json
+import subprocess
 
-. function.sh
-
-TMP1=`SCRIPTNAME`.log
-
-> $TMP1
-
-BAR
-
-CODE [U-50] 관리자 그룹에 최소한의 계정 포함
-
-cat << EOF >> $result
-
-양호: 관리자 그룹에 불필요한 계정이 등록되어 있지 않은 경우
-
-취약: 관리자 그룹에 불필요한 계정이 등록되어 있는 경우
-
-EOF
-
-BAR
+# 결과를 저장할 딕셔너리
+results = {
+    "U-50": {
+        "title": "관리자 그룹에 최소한의 계정 포함",
+        "status": "",
+        "description": {
+            "good": "관리자 그룹에 불필요한 계정이 등록되어 있지 않은 경우",
+            "bad": "관리자 그룹에 불필요한 계정이 등록되어 있는 경우"
+        },
+        "details": []
+    }
+}
 
 # 필요한 계정 목록 정의
-necessary_accounts=("root" "bin" "daemon" "adm" "lp" "sync" "shutdown" "halt" "adiosl" "mysql" "cubrid")
+necessary_accounts = ["root", "bin", "daemon", "adm", "lp", "sync", "shutdown", "halt", "adiosl", "mysql", "cubrid"]
 
-# 필요한 계정 목록에 없는 계정 검색
-unnecessary_accounts=$(getent group Administrators | awk -F: '{split($4,a,","); for(i in a) {if (!(a[i] in necessary_accounts)) { print a[i] }}}')
+def check_admin_group_accounts():
+    try:
+        # 관리자 그룹의 계정 목록 가져오기
+        proc = subprocess.Popen(['getent', 'group', 'root'], stdout=subprocess.PIPE)
+        stdout, _ = proc.communicate()
+        admin_accounts = stdout.decode().split(':')[-1].strip().split(',')
+        
+        # 불필요한 계정 검사
+        unnecessary_accounts = [acc for acc in admin_accounts if acc not in necessary_accounts]
 
-# 불필요한 계정이 발견되었는지 확인합니다
-if [ -n "$unnecessary_accounts" ]; then
-  WARN "Administrators 그룹에서 불필요한 계정이 발견되었습니다.: $unnecessary_accounts"
-else
-  OK "Administrators 그룹에서 불필요한 계정을 찾을 수 없습니다."
-fi
+        if unnecessary_accounts:
+            results["U-50"]["status"] = "취약"
+            results["U-50"]["details"].extend(unnecessary_accounts)
+        else:
+            results["U-50"]["status"] = "양호"
+            results["U-50"]["details"].append("관리자 그룹에 불필요한 계정이 없습니다.")
+    except Exception as e:
+        results["U-50"]["status"] = "에러"
+        results["U-50"]["details"].append(f"관리자 그룹 계정 검사 중 오류 발생: {str(e)}")
 
-cat $result
+check_admin_group_accounts()
 
-echo ; echo
+# 결과 파일에 JSON 형태로 저장
+result_file = 'admin_group_accounts_check_result.json'
+with open(result_file, 'w') as file:
+    json.dump(results, file, indent=4, ensure_ascii=False)
+
+# 결과 콘솔에 출력
+print(json.dumps(results, indent=4, ensure_ascii=False))
--- centos/U-51.py
+++ centos/U-51.py
@@ -1,64 +1,53 @@
-#!/bin/bash
+#!/usr/bin/env python3
+import json
+import subprocess
 
- 
+# 결과를 저장할 딕셔너리
+results = {
+    "U-51": {
+        "title": "계정이 존재하지 않는 GID 금지",
+        "status": "",
+        "description": {
+            "good": "존재하지 않는 계정에 GID 설정을 금지한 경우",
+            "bad": "존재하지 않은 계정에 GID 설정이 되어있는 경우"
+        },
+        "details": []
+    }
+}
 
-. function.sh
+# 필요한 그룹 목록
+necessary_groups = [
+    "root", "sudo", "sys", "adm", "wheel", "daemon", "bin", "lp", "dbus", 
+    "rpc", "rpcuser", "haldaemon", "apache", "postfix", "gdm", "adiosl", 
+    "mysql", "cubrid", "messagebus", "syslog", "avahi", "whoopsie", "colord", 
+    "systemd-network", "systemd-resolve", "systemd-timesync", "sync", "user", 
+    "tty", "disk", "mem", "kmem", "mail", "uucp", "man", "games", "gopher", 
+    "video", "dip", "ftp", "lock", "audio", "nobody", "users", "usbmuxd", 
+    "utmp", "utempter", "rtkit", "avahi-autoipd", "ssh", "nfsnobody", 
+    "stapdev", "mem", "kmem", "www-data", "sasl", "nogroup", "ssh", "nfsnobody",
+    "stapdev", "mem", "kmem"
+]
 
-TMP1=`SCRIPTNAME`.log
+def check_groups():
+    # 시스템에 존재하는 모든 그룹 가져오기
+    proc = subprocess.Popen(['getent', 'group'], stdout=subprocess.PIPE)
+    stdout, _ = proc.communicate()
+    all_groups = [line.split(':')[0] for line in stdout.decode().splitlines()]
 
+    # 필요하지 않은 그룹 검사
+    for group in all_groups:
+        if group not in necessary_groups:
+            results["U-51"]["details"].append(f"Group {group}은(는) 시스템 관리 또는 운영에 필요하지 않으므로 검토해야 합니다.")
+            results["U-51"]["status"] = "취약"
+        else:
+            results["U-51"]["status"] = "양호"
 
-BAR
+check_groups()
 
-CODE [U-51] 계정이 존재하지 않는 GID 금지
+# 결과 파일에 JSON 형태로 저장
+result_file = 'gid_check_result.json'
+with open(result_file, 'w') as file:
+    json.dump(results, file, indent=4, ensure_ascii=False)
 
-cat << EOF >> $result
-
-양호: 존재하지 않는 계정에 GID 설정을 금지한 경우
-
-취약: 존재하지 않은 계정에 GID 설정이 되어있는 경우
-
-EOF
-
-BAR
-
-
-declare -a necessary_groups=("root" "sudo" "sys" "adm" "wheel" 
-"daemon" "bin" "lp" "dbus" "rpc" "rpcuser" "haldaemon" 
-"apache" "postfix" "gdm" "adiosl" "mysql" "cubrid"
- "messagebus" "syslog" "avahi" "whoopsie"
-"colord" "systemd-network" "systemd-resolve"
-"systemd-timesync" "mysql" "sync" "user"
-"tty" "disk" "men" "kmen" "mail" "uucp"
-"man" "games" "gopher" "video" "dip"
-"ftp" "lock" "audio" "nobody" "users"
-"usbmuxd" "utmp" "utempter" "rtkit"
-"avahi-autoipd" "desktop_admin_r"
-"desktop_user_r" "floppy"
-"vcsa" "abrt" "cdrom" "tape"
-"dialout" "wbpriv" "nfsnonody"
-"ntp" "saslauth" "postdrop"
-"pulse" "pulse-access" "fuse" 
-"sshd" "slocate" "stapusr"
-"stapsys" "tcpdump" "named"
-"www-data" "sasl" "nogroup"
-"ssh" "nfsnobody" "stapdev"
-"mem" "kmem")
-
-
-all_groups=$(getent group | cut -d: -f1)
-
-for group in $all_groups; do
-  if ! [[ " ${necessary_groups[@]} " =~ " ${group} " ]]; then
-    WARN "Group ${group}은(는) 시스템 관리 또는 운영에 필요하지 않으므로 검토해야 합니다."
-  else
-    OK "Group ${group}은(는) 시스템 관리 또는 운영에 필요합니다."
-  fi
-done
-
-
-
- 
-
-cat $result
-
-echo ; echo
+# 결과 콘솔에 출력
+print(json.dumps(results, indent=4, ensure_ascii=False))
