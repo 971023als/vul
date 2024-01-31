88435be3e8a7171d79489cca924b30d9661fff9d
971023als <971023als@naver.com>
2024-01-31 21:34:36 +0900

    21 23번 수정

--- centos/U-23.py
+++ centos/U-23.py
@@ -1,109 +1,55 @@
 #!/bin/python3
 
-. function.sh
+import os
+import json
+import glob
 
-TMP1=`SCRIPTNAME`.log
+# 결과 리스트 초기화
+results = []
 
-> $TMP1
+# 검사할 서비스 목록
+services = ["echo", "discard", "daytime", "chargen"]
 
-BAR
+def check_service_status(service):
+    """
+    주어진 서비스에 대해 상태를 검사하고 결과를 반환합니다.
+    """
+    files = glob.glob(f"/etc/xinetd.d/{service}*")
+    if not files:
+        print(f"/etc/xinetd.d/{service} 파일이 존재하지 않습니다.")
+        return "OK", f"/etc/xinetd.d/{service} 파일이 존재하지 않습니다."
+    
+    for file in files:
+        print(f"{file} 파일이 존재합니다.")
+        with open(file) as f:
+            for line in f:
+                if "disable" in line:
+                    status = line.split()[-1]
+                    if status == "yes":
+                        print(f"{file} 파일에 대한 서비스가 비활성화 되어 있습니다.")
+                        return "OK", f"{file} 파일에 대한 서비스가 비활성화 되어 있습니다."
+                    else:
+                        print(f"{file} 파일에 대한 서비스가 활성화 되어 있습니다.")
+                        return "WARN", f"{file} 파일에 대한 서비스가 활성화 되어 있습니다."
+    return "N/A", "상태를 확인할 수 없습니다."
 
-CODE [U-23] DoS 공격에 취약한 서비스 비활성화
-
-cat << EOF >> $result
-
-[ 양호 ] : DoS 공격에 취약한 echo, discard, daytime, chargen 서비스가 비활성화 된 경우
-
-[ 취약 ] : DoS 공격에 취약한 echo, discard, daytime, chargen 서비스 활성화 된 경우
-
-EOF
-
-BAR
-
-ls /etc/xinetd.d/echo* >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
-OK /etc/xinetd.d/echo 파일이 존재하지 않습니다.
-else
-for i in `ls /etc/xinetd.d/echo*`
-do
-INFO $i 파일이 존재합니다.
-if [ `cat $i | grep disable | awk '{print $3}'` = yes ] ; then
-OK $i 파일에 대한 서비스가 비활성화 되어 있습니다.
-else
-WARN $i 파일에 대한 서비스가 활성화 되어 있습니다.
-fi
-done
-fi
-
-ls /etc/xinetd.d/discard* >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
-OK /etc/xinetd.d/discard 파일이 존재하지 않습니다.
-else
-for i in `ls /etc/xinetd.d/discard*`
-do
-INFO $i 파일이 존재합니다.
-if [ `cat $i | grep disable | awk '{print $3}'` = yes ] ; then
-OK $i 파일에 대한 서비스가 비활성화 되어 있습니다.
-else
-WARN $i 파일에 대한 서비스가 활성화 되어 있습니다.
-fi
-done
-fi
-
-
-ls /etc/xinetd.d/daytime* >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
-OK /etc/xinetd.d/daytime 파일이 존재하지 않습니다.
-else
-for i in `ls /etc/xinetd.d/daytime*`
-do
-INFO $i 파일이 존재합니다.
-if [ `cat $i | grep disable | awk '{print $3}'` = yes ] ; then
-OK $i 파일에 대한 서비스가 비활성화 되어 있습니다.
-else
-WARN $i 파일에 대한 서비스가 활성화 되어 있습니다.
-fi
-done
-fi
-
-ls /etc/xinetd.d/chargen* >/dev/null 2>&1
-if [ $? -ne 0 ] ; then
-OK /etc/xinetd.d/chargen 파일이 존재하지 않습니다.
-else
-for i in `ls /etc/xinetd.d/chargen*`
-do
-INFO $i 파일이 존재합니다.
-if [ `cat $i | grep disable | awk '{print $3}'` = yes ] ; then
-OK $i 파일에 대한 서비스가 비활성화 되어 있습니다.
-else
-WARN $i 파일에 대한 서비스가 활성화 되어 있습니다.
-fi
-done
-fi
-
-cat $result
-
-echo ; echo
-
-if nonexistent_device_files:
-        results.append({
-            "분류": "서비스 관리",
-            "코드": "U-23",
-            "위험도": "상",
-            "진단 항목": "DoS 공격에 취약한 서비스 비활성화",
-            "진단 결과": "취약",
-            "현황": " xinted 사용시에 파일의 소유자 및 권한 설정 부족",
-            "대응방안": " xinted 사용시에 파일의 소유자 및 권한 설정"
-        })
+# 각 서비스에 대해 검사 실행
+for service in services:
+    status, message = check_service_status(service)
+    if status == "OK":
+        result_status = "양호"
     else:
-        results.append({
-            "분류": "서비스 관리",
-            "코드": "U-23",
-            "위험도": "상",
-            "진단 항목": "DoS 공격에 취약한 서비스 비활성화",
-            "진단 결과": "양호",
-            "현황": " xinted 사용시에 파일의 소유자 및 권한 설정 부족",
-            "대응방안": "  xinted 사용시에 파일의 소유자 및 권한 설정"
-        })
+        result_status = "취약"
+    
+    results.append({
+        "분류": "서비스 관리",
+        "코드": "U-23",
+        "위험도": "상",
+        "진단 항목": f"DoS 공격에 취약한 {service} 서비스 비활성화",
+        "진단 결과": result_status,
+        "현황": message,
+        "대응방안": "xinetd 사용시에 파일의 소유자 및 권한 설정"
+    })
 
-return results
+# 결과 출력
+print(json.dumps(results, ensure_ascii=False, indent=4))
