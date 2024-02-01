818ae8d4e7d06f986917a6e60ba8a2ab153d48f2
971023als <971023als@naver.com>
2024-02-01 11:16:45 +0900

    14 21번 수정

--- ubuntu/U-21.py
+++ ubuntu/U-21.py
@@ -1,64 +1,53 @@
 #!/usr/bin/python3
 
-import os
-import json
+. function.sh
 
-# 결과를 저장할 딕셔너리
-results = {
-    "U-21": {
-        "title": "r 계열 서비스 비활성화",
-        "status": "",
-        "description": {
-            "good": "r 계열 서비스가 비활성화 되어 있는 경우",
-            "bad": "r 계열 서비스가 활성화 되어 있는 경우",
-        },
-        "files": []
-    }
-}
+TMP1=`SCRIPTNAME`.log
 
-expected_settings = [
-    "socket_type= stream",
-    "wait= no",
-    "user= nobody",
-    "log_on_success+= USERID",
-    "log_on_failure+= USERID",
-    "server= /usr/sdin/in.fingerd",
-    "disable= yes"
-]
+> $TMP1
+ 
 
-def check_r_services():
-    files = ["/etc/xinetd.d/rlogin", "/etc/xinetd.d/rsh", "/etc/xinetd.d/rexec"]
-    all_settings_correct = True
-    
-    for file_path in files:
-        if not os.path.exists(file_path):
-            results["files"].append(f"{file_path} 파일이 없습니다.")
-            continue
+BAR
 
-        with open(file_path, 'r') as file:
-            file_contents = file.read()
-            file_results = {"file": file_path, "issues": []}
-            
-            for setting in expected_settings:
-                if setting not in file_contents:
-                    all_settings_correct = False
-                    file_results["issues"].append(f"'{setting}'이 올바르게 설정되지 않았습니다.")
+CODE [U-21] r 계열 서비스 비활성화
 
-            results["files"].append(file_results)
+cat << EOF >> $result
 
-    if all_settings_correct:
-        results["U-21"]["status"] = "양호"
-        results["U-21"]["message"] = results["U-21"]["description"]["good"]
-    else:
-        results["U-21"]["status"] = "취약"
-        results["U-21"]["message"] = results["U-21"]["description"]["bad"]
+[양호]: r 계열 서비스가 비활성화 되어 있는 경우
 
-# 검사 수행
-check_r_services()
+[취약]: r 계열 서비스가 활성화 되어 있는 경우
 
-# 결과를 JSON 파일로 저장
-with open('result.json', 'w', encoding='utf-8') as f:
-    json.dump(results, f, ensure_ascii=False, indent=4)
+EOF
 
-# 결과 출력
-print(json.dumps(results, ensure_ascii=False, indent=4))
+BAR
+
+
+files=(/etc/xinetd.d/rlogin /etc/xinetd.d/rsh /etc/xinetd.d/rexec)
+expected_settings=(
+"socket_type= stream"
+"wait= no"
+"user= nobody"
+"log_on_success+= USERID"
+"log_on_failure+= USERID"
+"server= /usr/sdin/in.fingerd"
+"disable= yes"
+)
+
+for file in "${files[@]}"; do
+  INFO "파일 확인 중: $file"
+  if [ ! -f "$file" ]; then
+	  INFO "$file 파일이 없습니다."
+  else
+    for setting in "${expected_settings[@]}"; do
+      if grep -q "$setting" "$file"; then
+        OK "'$setting'이 올바르게 설정되었습니다."
+      else
+        WARN "$file 파일에서 '$setting'을 올바르게 설정하지 않았습니다."
+      fi
+    done
+  fi
+done
+
+cat $result
+
+echo ; echo
