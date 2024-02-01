ab69ad5c59f6c6a6da6222c5b787edb810150674
971023als <971023als@naver.com>
2024-02-01 19:09:21 +0900

    49 50 51번 수정

--- centos/U-50.py
+++ centos/U-50.py
@@ -1,30 +1,37 @@
-#!/bin/python3
 
-import subprocess
-import json
+. function.sh
+
+TMP1=`SCRIPTNAME`.log
+
+> $TMP1
+
+BAR
+
+CODE [U-50] 관리자 그룹에 최소한의 계정 포함
+
+cat << EOF >> $result
+
+양호: 관리자 그룹에 불필요한 계정이 등록되어 있지 않은 경우
+
+취약: 관리자 그룹에 불필요한 계정이 등록되어 있는 경우
+
+EOF
+
+BAR
 
 # 필요한 계정 목록 정의
-necessary_accounts = set(["root", "bin", "daemon", "adm", "lp", "sync", "shutdown", "halt", "adiosl", "mysql", "cubrid"])
+necessary_accounts=("root" "bin" "daemon" "adm" "lp" "sync" "shutdown" "halt" "adiosl" "mysql" "cubrid")
 
-# 관리자 그룹의 계정 목록을 가져옵니다
-try:
-    admin_group_accounts = subprocess.check_output("getent group root", shell=True, text=True).strip().split(":")[-1].split(",")
-except subprocess.CalledProcessError:
-    admin_group_accounts = []
+# 필요한 계정 목록에 없는 계정 검색
+unnecessary_accounts=$(getent group Administrators | awk -F: '{split($4,a,","); for(i in a) {if (!(a[i] in necessary_accounts)) { print a[i] }}}')
 
-# 필요한 계정 목록에 없는 계정을 식별합니다
-unnecessary_accounts = [account for account in admin_group_accounts if account not in necessary_accounts]
+# 불필요한 계정이 발견되었는지 확인합니다
+if [ -n "$unnecessary_accounts" ]; then
+  WARN "Administrators 그룹에서 불필요한 계정이 발견되었습니다.: $unnecessary_accounts"
+else
+  OK "Administrators 그룹에서 불필요한 계정을 찾을 수 없습니다."
+fi
 
-# 결과 저장을 위한 딕셔너리
-results = {
-    "분류": "서비스 관리",
-    "코드": "U-50",
-    "위험도": "상",
-    "진단 항목": "관리자 그룹에 최소한의 계정 포함",
-    "진단 결과": "취약" if unnecessary_accounts else "양호",
-    "현황": f"관리자 그룹에 불필요한 계정이 등록되어 있습니다: {', '.join(unnecessary_accounts)}" if unnecessary_accounts else "관리자 그룹에 불필요한 계정이 등록되어 있지 않습니다.",
-    "대응방안": "관리자 그룹에서 불필요한 계정을 제거하십시오." if unnecessary_accounts else "현재 설정 유지"
-}
+cat $result
 
-# 결과 출력
-print(json.dumps(results, ensure_ascii=False, indent=4))
+echo ; echo
