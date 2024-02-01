3013b14f2c53b11c674bfacb8edae3dfa2bf9a76
971023als <971023als@naver.com>
2024-02-01 19:17:13 +0900

    Update U-35.py

--- centos/U-35.py
+++ centos/U-35.py
@@ -1,41 +1,35 @@
+#!/usr/bin/python3
+import subprocess
+import json
 
-. function.sh
+def check_apache_directory_listing():
+    results = {
+        "분류": "웹 서버 설정",
+        "코드": "U-35",
+        "위험도": "상",
+        "진단 항목": "Apache 디렉터리 리스팅 제거",
+        "진단 결과": "",
+        "현황": [],
+        "대응방안": "[양호]: 디렉터리 검색 기능을 사용하지 않는 경우\n[취약]: 디렉터리 검색 기능을 사용하는 경우"
+    }
 
-TMP1=`SCRIPTNAME`.log
+    config_file = "/etc/httpd/conf/httpd.conf"
+    try:
+        with open(config_file, 'r') as file:
+            if 'Options Indexes' in file.read():
+                results["진단 결과"] = "취약"
+                results["현황"].append("Apache2 서버에서 디렉터리 목록이 사용 가능합니다.")
+            else:
+                results["진단 결과"] = "양호"
+                results["현황"].append("Apache2 서버에서 디렉터리 목록이 사용 가능하지 않습니다.")
+    except Exception as e:
+        results["현황"].append(f"{config_file} 파일 열기 오류: {str(e)}")
 
-> $TMP1  
+    return results
 
- 
+def main():
+    results = check_apache_directory_listing()
+    print(json.dumps(results, ensure_ascii=False, indent=4))
 
-BAR
-
-CODE [U-35] Apache 디렉터리 리스팅 제거
-
-cat << EOF >> $result
-
-[양호]: 디렉터리 검색 기능을 사용하지 않는 경우
-
-[취약]: 디렉터리 검색 기능을 사용하는 경우
-
-EOF
-
-BAR
-
-
-
-# Apache2 구성 파일 경로 설정
-config_file="/etc/httpd/conf/httpd.conf"
-
-# grep을 사용하여 구성 파일에서 디렉토리 목록이 사용 가능한지 확인합니다
-Result=$(grep -E "^[ \t]*Options[ \t]+Indexes" $config_file)
-
-if [ -n "$Result" ]; then
-    WARN "Apache2 서버에서 디렉터리 목록이 사용 가능합니다."
-else
-    OK "Apache2 서버에서 디렉터리 목록이 사용 가능하지 않습니다."
-fi
-
-
-cat $result
-
-echo ; echo
+if __name__ == "__main__":
+    main()
