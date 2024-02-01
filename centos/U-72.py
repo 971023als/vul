0e45f8f468bea51abc18cd97f50c5780b496a6e1
971023als <971023als@naver.com>
2024-02-01 20:42:12 +0900

    Update U-72.py

--- centos/U-72.py
+++ centos/U-72.py
@@ -1,52 +1,47 @@
-#!/bin/python3
-
-import os
+#!/usr/bin/python3
 import json
 
-# 로그 파일과 로그 구성 파일 위치 정의
-log_files = [
-    "/var/log/secure",
-    "/var/log/messages",
-    "/var/log/audit/audit.log",
-    "/var/log/httpd/access_log",
-    "/var/log/httpd/error_log"
-]
+def check_system_logging_policy():
+    results = {
+        "분류": "시스템 설정",
+        "코드": "U-72",
+        "위험도": "상",
+        "진단 항목": "정책에 따른 시스템 로깅 설정",
+        "진단 결과": "",
+        "현황": [],
+        "대응방안": "[양호]: 로그 기록 정책이 정책에 따라 설정되어 수립되어 있는 경우\n[취약]: 로그 기록 정책이 정책에 따라 설정되어 수립되어 있지 않은 경우"
+    }
 
-conf_files = [
-    "/etc/rsyslog.conf",
-    "/etc/httpd/conf/httpd.conf",
-    "/etc/audit/auditd.conf"
-]
+    filename = "/etc/rsyslog.conf"
+    expected_content = [
+        "*.info;mail.none;authpriv.none;cron.none /var/log/messages",
+        "authpriv.* /var/log/secure",
+        "mail.* /var/log/maillog",
+        "cron.* /var/log/cron",
+        "*.alert /dev/console",
+        "*.emerg *"
+    ]
 
-# 결과 저장을 위한 딕셔너리
-results = {
-    "분류": "서비스 관리",
-    "코드": "U-72",
-    "위험도": "상",
-    "진단 항목": "정책에 따른 시스템 로깅 설정",
-    "현황": [],
-    "대응방안": "모든 필수 로그 파일과 로그 구성 파일이 존재하도록 설정하세요."
-}
-
-# 로그 파일 존재 여부 검사
-for file in log_files:
-    if os.path.exists(file):
-        results["현황"].append(f"{file} 파일이 존재합니다.")
-    else:
-        results["현황"].append(f"{file} 파일이 존재하지 않습니다.")
+    try:
+        with open(filename, 'r') as file:
+            content = file.read()
+            match_count = sum(1 for line in expected_content if line in content)
+            
+            if match_count == len(expected_content):
+                results["진단 결과"] = "양호"
+                results["현황"].append(f"{filename}의 내용이 정확합니다.")
+            else:
+                results["진단 결과"] = "취약"
+                results["현황"].append(f"{filename}의 내용이 잘못되었습니다.")
+    except FileNotFoundError:
         results["진단 결과"] = "취약"
+        results["현황"].append(f"{filename} 파일이 존재하지 않습니다.")
 
-# 로그 구성 파일 존재 여부 검사
-for file in conf_files:
-    if os.path.exists(file):
-        results["현황"].append(f"{file} 구성 파일이 존재합니다.")
-    else:
-        results["현황"].append(f"{file} 구성 파일이 존재하지 않습니다.")
-        results["진단 결과"] = "취약"
+    return results
 
-# 모든 필수 파일이 존재하는 경우 진단 결과를 양호로 설정
-if "진단 결과" not in results:
-    results["진단 결과"] = "양호"
+def main():
+    results = check_system_logging_policy()
+    print(json.dumps(results, ensure_ascii=False, indent=4))
 
-# 결과 출력
-print(json.dumps(results, ensure_ascii=False, indent=4))
+if __name__ == "__main__":
+    main()
