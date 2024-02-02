1aa3dd0f6e44a3b7b2b50f26933d4da9816349cc
971023als <971023als@naver.com>
2024-02-03 00:33:57 +0900

    59번 수정

--- linux/Python_json/Rocky/U-59.py
+++ linux/Python_json/Rocky/U-59.py
@@ -8,7 +8,7 @@ def search_hidden_files_and_directories(start_path):
         "코드": "U-59",
         "위험도": "하",
         "진단 항목": "숨겨진 파일 및 디렉토리 검색 및 제거",
-        "진단 결과": "양호",  # Assume "Good" until proven otherwise
+        "진단 결과": "",  # 초기 값 설정하지 않음
         "현황": {"숨겨진 파일": [], "숨겨진 디렉터리": []},
         "대응방안": "불필요하거나 의심스러운 숨겨진 파일 및 디렉터리 삭제"
     }
@@ -19,17 +19,18 @@ def search_hidden_files_and_directories(start_path):
         for file in files:
             if file.startswith('.'):
                 results["현황"]["숨겨진 파일"].append(os.path.join(root, file))
-                results["진단 결과"] = "취약"
         
         # Check each directory
         for dir in dirs:
             if dir.startswith('.'):
                 results["현황"]["숨겨진 디렉터리"].append(os.path.join(root, dir))
-                results["진단 결과"] = "취약"
 
-    if results["진단 결과"] == "양호":
+    # 진단 결과 업데이트
+    if not results["현황"]["숨겨진 파일"] and not results["현황"]["숨겨진 디렉터리"]:
+        results["진단 결과"] = "양호"
         results["현황"] = "숨겨진 파일이나 디렉터리가 없습니다."
     else:
+        results["진단 결과"] = "취약"
         if not results["현황"]["숨겨진 파일"]:
             del results["현황"]["숨겨진 파일"]
         if not results["현황"]["숨겨진 디렉터리"]:
@@ -41,7 +42,7 @@ def main():
     # Example: search in the current user's home directory
     home_directory = os.path.expanduser('~')
     hidden_items_check_results = search_hidden_files_and_directories(home_directory)
-    print(hidden_items_check_results)
+    print(json.dumps(hidden_items_check_results, ensure_ascii=False, indent=4))
 
 if __name__ == "__main__":
     main()
