9a4e5351153f056c35f55630b3cba735bb424d27
971023als <971023als@naver.com>
2024-02-03 00:53:07 +0900

    14 22번 수정

--- linux/Python_json/centos/U-22.py
+++ linux/Python_json/centos/U-22.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import os
 import stat
+import json
 
 def check_cron_permissions():
     results = {
--- linux/Python_json/ubuntu/U-14.py
+++ linux/Python_json/ubuntu/U-14.py
@@ -10,7 +10,7 @@ def check_user_system_start_files():
         "코드": "U-14",
         "위험도": "상",
         "진단 항목": "사용자, 시스템 시작파일 및 환경파일 소유자 및 권한 설정",
-        "진단 결과": "양호",  # 초기 상태를 "양호"로 설정
+        "진단 결과": "",  # 초기 진단 결과 설정하지 않음
         "현황": [],
         "대응방안": "홈 디렉터리 환경변수 파일 소유자가 root 또는 해당 계정으로 지정되어 있고, 쓰기 권한이 부여된 경우"
     }
@@ -26,26 +26,24 @@ def check_user_system_start_files():
             file_path = os.path.join(home, start_file)
             if os.path.isfile(file_path):
                 file_stat = os.stat(file_path)
-                owner_uid = file_stat.st_uid
-                owner_name = pwd.getpwuid(owner_uid).pw_name
                 mode = file_stat.st_mode
 
                 # 파일 소유자가 root 또는 해당 사용자가 아니거나, 다른 사용자에게 쓰기 권한이 있을 경우
-                if not ((owner_name == 'root' or owner_name == os.path.basename(home)) and not (mode & stat.S_IWOTH)):
+                if not (file_stat.st_uid == 0 or file_stat.st_uid == pwd.getpwnam(os.path.basename(home)).pw_uid) or (mode & stat.S_IWOTH):
                     vulnerable_files.append(file_path)
 
     if vulnerable_files:
         results["진단 결과"] = "취약"
         results["현황"] = vulnerable_files
     else:
-        # 취약한 파일이 없을 경우 "현황"에 안내 메시지 추가
+        results["진단 결과"] = "양호"
         results["현황"].append("모든 홈 디렉터리 내 시작파일 및 환경파일이 적절한 소유자와 권한 설정을 가지고 있습니다.")
 
     return results
 
 def main():
-    results = check_user_system_start_files()
-    print(json.dumps(results, ensure_ascii=False, indent=4))
+    user_system_start_files_check_results = check_user_system_start_files()
+    print(json.dumps(user_system_start_files_check_results, ensure_ascii=False, indent=4))
 
 if __name__ == "__main__":
     main()
