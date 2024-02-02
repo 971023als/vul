40dd53eefe6c526aa0a591e2f3ed8081f25183b6
971023als <971023als@naver.com>
2024-02-02 20:28:46 +0900

    록키 업데이트

---
+++ linux/shell scirpt_txt/Rocky/check_perm.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+function print_good () {
+    echo -e "\x1B[01;32m[+]\x1B[0m $1"
+}
+
+function print_error () {
+    echo -e "\x1B[01;31m[-]\x1B[0m $1"
+}
+
+function print_info () {
+    echo -e "\x1B[01;34m[*]\x1B[0m $1"
+}
+
+function OK () {
+    echo -e "\x1B[01;32m[ 양호 ]\x1B[0m $*"
+}
+
+function WARN () {
+    echo -e "\x1B[01;31m[ 취약 ]\x1B[0m $*"
+}
+
+function INFO () {
+    echo -e "\x1B[01;34m[ 정보 ]\x1B[0m $1"
+}
+
+function ERROR () {
+    echo -e "\x1B[01;31m[ 애러 ]\x1B[0m $1"
+}
+
+
+# 1) 변수 설정 및 초기화
+PermList=perm.list # 점검 대상 파일 목록
+Result=report2.txt # 점검 결과
+TMP1=/tmp/tmp1
+
+> $Result # 파일 실행 시 점검 결과 초기화
+> $TMP1 # 파일 실행 시 임시 파일 초기화
+
+# 2) 파일 유무 점검
+for ChkFile in `cat $PermList | awk '{print $1}'`
+do
+if [ ! -f $ChkFile ] ; then
+echo "[ WARN ] '$ChkFile' not found."
+exit 1
+break
+fi
+done
+
+
+# 3) 점검 스크립트
+cat $PermList | while read FName FAuth FPermNum FPermChar
+do
+if [ ! -f $FName ] ; then
+echo "[ ERROR ] $FName not found" >> $Result
+echo >> $Result
+continue
+else
+find $FName -type f -perm -$FPermNum -ls | fgrep -v $FPermChar > $TMP1
+if [ -s $TMP1 ] ; then
+BadPerm=$(cat $TMP1 | awk '{print $3}')
+WARN "$FPermNum : $FName ($BadPerm)" >> $Result
+else
+GoodPerm=$(find $FName -type f -ls | awk '{print $3}')
+OK "$FPermNum : $FName ($GoodPerm)" >> $Result
+fi
+fi
+done
+
+
+ 
