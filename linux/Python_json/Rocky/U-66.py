0e997e1fa2aa6aa956bf67d68635972136b9c2e6
971023als <971023als@naver.com>
2024-02-03 00:41:45 +0900

    66번 수정

--- linux/Python_json/Rocky/U-66.py
+++ linux/Python_json/Rocky/U-66.py
@@ -8,8 +8,8 @@ def check_snmp_service_usage():
         "코드": "U-66",
         "위험도": "중",
         "진단 항목": "SNMP 서비스 구동 점검",
-        "진단 결과": "양호",  # Assume "Good" until proven otherwise
-        "현황": "SNMP 서비스를 사용하지 않고 있습니다.",
+        "진단 결과": "",  # 초기 값 설정하지 않음
+        "현황": "",
         "대응방안": "SNMP 서비스 사용을 필요로 하지 않는 경우, 서비스를 비활성화"
     }
 
@@ -18,12 +18,15 @@ def check_snmp_service_usage():
     if 'snmp' in process.stdout.lower():
         results["진단 결과"] = "취약"
         results["현황"] = "SNMP 서비스를 사용하고 있습니다."
+    else:
+        results["진단 결과"] = "양호"
+        results["현황"] = "SNMP 서비스를 사용하지 않고 있습니다."
 
     return results
 
 def main():
     snmp_service_check_results = check_snmp_service_usage()
-    print(snmp_service_check_results)
+    print(json.dumps(snmp_service_check_results, ensure_ascii=False, indent=4))
 
 if __name__ == "__main__":
     main()
