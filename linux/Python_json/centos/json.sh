f158f68ac8fa28f18790554c65608f745d5926a3
971023als <971023als@naver.com>
2024-02-05 02:48:59 +0900

    json

--- linux/Python_json/centos/json.sh
+++ linux/Python_json/centos/json.sh
@@ -10,7 +10,7 @@ HTML_PATH="/var/www/html/index.php"
 echo "{" > "$RESULTS_PATH"
 errors=()
 
-# U-01.py부터 U-72.py까지 실행
+# JSON 파일을 생성하는 Bash 스크립트 부분
 for i in $(seq -w 1 72); do
     script_name="U-${i}.py"
     start_time=$(date +%s.%N)
@@ -18,21 +18,18 @@ for i in $(seq -w 1 72); do
     end_time=$(date +%s.%N)
     execution_time=$(echo "$end_time - $start_time" | bc)
 
-    # JSON 문자열을 Bash에서 직접 생성할 때 빈 값이 있는 경우를 처리
-    output_escaped=$(echo "$output" | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
-
-    # output이 비어 있는 경우를 확인하고, 빈 문자열을 할당
-    if [ -z "$output_escaped" ]; then
+    # output 변수의 내용을 JSON 문자열에 안전하게 포함하기 위한 처리
+    # output 값이 비어 있는 경우 빈 문자열을 할당
+    if [ -z "$output" ]; then
         output_escaped="\"\""
+    else
+        output_escaped=$(echo "$output" | sed 's/"/\\"/g' | sed ':a;N;$!ba;s/\n/\\n/g')
     fi
 
-    printf "\"$i\": {\"output\": %s, \"execution_time\": \"%s\"},\n" "$output_escaped" "$execution_time" >> "$RESULTS_PATH"
-
-
-    if [[ $output == *ERROR* ]]; then
-        errors+=("$script_name: $output")
-    fi
+    # JSON 구조에 output_escaped 값을 포함시키기
+    printf "\"$i\": {\"output\": \"%s\", \"execution_time\": \"%f\"},\n" "$output_escaped" "$execution_time" >> "$RESULTS_PATH"
 done
+
 
 # 파일 마지막에 JSON 닫기
 sed -i '$ s/,$/\n}/' "$RESULTS_PATH" # 마지막 쉼표를 닫는 중괄호로 안전하게 변경
