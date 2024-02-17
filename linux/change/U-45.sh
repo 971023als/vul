6f284fcd21c7dec161057e41b304741d735ebf83
971023als <971023als@naver.com>
2024-02-18 00:58:27 +0900

    45 46 변경

--- linux/change/U-45.sh
+++ linux/change/U-45.sh
@@ -1,39 +1,18 @@
 #!/bin/bash
 
-# 결과를 저장할 JSON 파일 초기화
-results_file="results.json"
-echo '{
-    "분류": "계정관리",
-    "코드": "U-45",
-    "위험도": "하",
-    "진단 항목": "root 계정 su 제한",
-    "진단 결과": "양호",
-    "현황": [],
-    "대응방안": "su 명령어 사용 특정 그룹 제한"
-}' > $results_file
-
+# /etc/pam.d/su 파일 검사
 pam_su_path="/etc/pam.d/su"
 
+# 파일 존재 여부 및 설정 검사
 if [ -f "$pam_su_path" ]; then
-    pam_contents=$(cat "$pam_su_path")
-    if echo "$pam_contents" | grep -q "pam_rootok.so"; then
-        if ! echo "$pam_contents" | grep -q "pam_wheel.so" || ! echo "$pam_contents" | grep -q "auth required pam_wheel.so use_uid"; then
-            result="취약"
-            status="/etc/pam.d/su 파일에 pam_wheel.so 모듈 설정이 적절히 구성되지 않았습니다."
-        fi
+    # pam_wheel.so 모듈의 적절한 설정 확인
+    if grep -q "auth\s*required\s*pam_wheel.so\s*use_uid" "$pam_su_path"; then
+        jq '.진단 결과 = "양호" | .현황 += ["su 명령어 사용이 특정 그룹으로 제한되어 있습니다."]' $results_file > tmp.$$.json && mv tmp.$$.json $results_file
     else
-        result="취약"
-        status="/etc/pam.d/su 파일에서 pam_rootok.so 모듈이 누락되었습니다."
+        jq '.진단 결과 = "취약" | .현황 += ["/etc/pam.d/su 파일에 pam_wheel.so 모듈 설정이 적절히 구성되지 않았습니다."]' $results_file > tmp.$$.json && mv tmp.$$.json $results_file
     fi
 else
-    result="취약"
-    status="/etc/pam.d/su 파일이 존재하지 않습니다."
-fi
-
-if [ "$result" = "취약" ]; then
-    jq --arg status "$status" '.진단 결과 = "취약" | .현황 += [$status]' $results_file > tmp.$$.json && mv tmp.$$.json $results_file
-else
-    jq '.현황 += ["/etc/pam.d/su 파일에 대한 설정이 적절하게 구성되어 있습니다."]' $results_file > tmp.$$.json && mv tmp.$$.json $results_file
+    jq '.진단 결과 = "취약" | .현황 += ["/etc/pam.d/su 파일이 존재하지 않습니다."]' $results_file > tmp.$$.json && mv tmp.$$.json $results_file
 fi
 
 # 결과 출력
